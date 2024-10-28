014c164fe7a597605f91584c500927fcadc8e61b
michelleyeoh <michellew.yeoh@gmail.com>
2024-10-28 10:48:56 -0700

    with auth

--- app/(api)/api/events/route.ts
+++ app/(api)/api/events/route.ts
@@ -1,8 +1,9 @@
-import authenticated from '@utils/authentication/authenticated';
-import { POST as post } from './post';
-import { GET as get } from './get';
+// import authenticated from '@utils/authentication/authenticated';
+import { POST } from './post';
+import { GET } from './get';
 
-const POST = authenticated(post);
-const GET = authenticated(get);
+//commented out for testing
+// const POST = authenticated(post);
+// const GET = authenticated(get);
 
 export { POST, GET };
