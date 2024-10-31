5ed9625a17ec194a784dbb55976f3d863a518e4b
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-10-30 20:41:12 -0700

    Update get.ts

--- app/(api)/api/user-to-event/get.ts
+++ app/(api)/api/user-to-event/get.ts
@@ -1,8 +1,19 @@
-import { NextRequest } from 'next/server';
-import { GetManyJudges } from '@datalib/judges/getJudge';
+import { NextRequest, NextResponse } from 'next/server';
+import { GetUserToEvent } from '@datalib/user-to-event/getUserToEvent';
 import getQueries from '@utils/request/getQueries';
 
 export async function GET(request: NextRequest) {
-  const queries = await getQueries(request, 'judges');
-  return GetManyJudges(queries);
+  const queries = await getQueries(request, 'events');
+  
+  const id = queries.id as string;
+  const idType = queries.idType as 'user_id' | 'event_id';
+
+  if (!id || !idType) {
+    return NextResponse.json(
+      { ok: false, body: null, error: "Missing 'id' or 'idType' in query parameters" },
+      { status: 400 }
+    );
+  }
+
+  return await GetUserToEvent(id, idType);
 }
