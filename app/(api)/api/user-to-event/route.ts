ba4bcb9dc28ca1bece51bfa42969be0c58b353e8
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-10-30 20:43:52 -0700

    Updated route.ts

--- app/(api)/api/user-to-event/route.ts
+++ app/(api)/api/user-to-event/route.ts
@@ -1,8 +1,8 @@
-import authenticated from '@utils/authentication/authenticated';
-import { POST as post } from './post';
-import { GET as get } from './get';
+// import authenticated from '@utils/authentication/authenticated';
+import { POST } from './post';
+import { GET } from './get';
 
-const POST = authenticated(post);
-const GET = authenticated(get);
+// const POST = authenticated(post);
+// const GET = authenticated(get);
 
 export { POST, GET };
