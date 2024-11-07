dbceb9fb18537d8a2059e14800ac8e83db6b4d18
Win Cheng <winzamark12@gmail.com>
2024-11-06 17:31:14 -0800

    Added a waiting tips component

--- app/(pages)/judging-app/_components/JudgingHub/HubHero.tsx
+++ app/(pages)/judging-app/_components/JudgingHub/HubHero.tsx
@@ -1,5 +1,4 @@
 import JudgeInt from '@typeDefs/judge';
-import styles from './HubHero.module.scss';
 import Image from 'next/image';
 import judgeHeroes from '/public/judges/hub/judgingheroes.svg';
 
@@ -15,28 +14,28 @@ export default function HubHero({
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.welcome_text}>
-        <div className={styles.name_container}>
-          <h1>Welcome!</h1>
-          <p>
+    <div className="tw-w-full tw-overflow-hidden tw-bg-[#f2f2f7] tw-flex tw-flex-col tw-justify-center tw-gap-4 tw-relative">
+      <div className="tw-px-[5%] tw-pt-[50px] tw-pb-0 tw-flex tw-flex-col tw-gap-2 tw-z-[1]">
+        <div className="tw-w-full tw-flex tw-flex-col tw-text-start">
+          <h1 className="tw-font-bold">Welcome!</h1>
+          <p className="tw-text-[1.5rem]">
             We appreciate you for helping us judge one of California's biggest
             hackathons!
           </p>
         </div>
       </div>
-      <div className={styles.judgecontainer}>
-        <div className={styles.judgingheroes}>
+      <div>
+        <div className="tw-items-center tw-flex tw-justify-center">
           <Image
             src={judgeHeroes}
             alt="Judging Animals"
-            className={styles.bottom_blurb}
+            className="tw-right-0 tw-bottom-0"
           />
         </div>
-        <div className={styles.blurb}>
-          {/* <p className={styles.intro_text}>You're paired with...</p>
+        <div className="tw-w-[45%] tw-flex tw-flex-col tw-gap-4 tw-p-[11px] tw-right-4 tw-top-4 tw-rounded-[var(--b-radius)]">
+          {/* <p className="tw-text-base tw-font-medium">You're paired with...</p>
           {members.map((member: string, index: number) => (
-            <p key={index} className={styles.name}>
+            <p key={index} className="tw-text-[1.75rem] tw-font-semibold">
               {member}
             </p>
           ))} */}
