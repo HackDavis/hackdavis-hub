da62b1711218a90eac89556552e4b5a28d2658d3
Win Cheng <winzamark12@gmail.com>
2024-11-12 19:31:14 -0800

    removed a lot of stuff XD

--- app/(api)/_actions/judgeGroupsToTeams/deleteJudgeGroupToTeam.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { DeleteManyJudgeGroupsToTeams } from '@datalib/judgeGroups/deleteJudgeGroupToTeam';
-import parseAndReplace from '@utils/request/parseAndReplace';
-
-export default async function deleteManyJudgeGroupsToTeams(query: object = {}) {
-  const newQuery = await parseAndReplace(query);
-  await DeleteManyJudgeGroupsToTeams(newQuery);
-}
--- app/(api)/api/judge-group-to-teams/route.ts
+++
@@ -1,6 +0,0 @@
-import authenticated from '@utils/authentication/authenticated';
-import { GET as get } from './get';
-
-const GET = authenticated(get);
-
-export { GET };
