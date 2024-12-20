6b05724830b1f5850f75d6029169ccbc6892713b
brandonw504 <brandonw504@outlook.com>
2024-12-19 23:23:23 -0800

    Forgot to secure one route

--- app/(api)/api/users/[id]/[event_id]/route.ts
+++ app/(api)/api/users/[id]/[event_id]/route.ts
@@ -1,4 +1,8 @@
-import { POST } from './post';
-import { DELETE } from './delete';
+import authenticated from '@utils/authentication/authenticated';
+import { POST as post } from './post';
+import { DELETE as del } from './delete';
+
+const POST = authenticated(post);
+const DELETE = authenticated(del);
 
 export { POST, DELETE };
