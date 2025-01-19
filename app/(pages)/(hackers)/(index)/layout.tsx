b91a4b708a1d5a3f8f0b15ac4961bdd08ec9bcfd
Win Cheng <winzamark12@gmail.com>
2025-01-18 16:42:06 -0800

    StarterKitSlide for use in starterkit

--- app/(pages)/(hackers)/(index)/layout.tsx
+++ app/(pages)/(hackers)/(index)/layout.tsx
@@ -1,6 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 
-import ProtectedDisplay from '@components/ProtectedDisplay/ProtectedDisplay';
+import ProtectedDisplay from '../../_components/ProtectedDisplay/ProtectedDisplay';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
