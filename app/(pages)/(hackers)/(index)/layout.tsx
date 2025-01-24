2792f41c6108f72e07cffd51c246246eb4f08212
Haylie Tan <haylie.tan.code@gmail.com>
2025-01-23 17:52:33 -0800

    Testing StarterKitSlide

--- app/(pages)/(hackers)/(index)/layout.tsx
+++ app/(pages)/(hackers)/(index)/layout.tsx
@@ -1,16 +1,21 @@
 import { SessionProvider } from 'next-auth/react';
+import StarterKitSlide from './starter-kit/_components/StarterKitSlide';
 
 import ProtectedDisplay from '../../_components/ProtectedDisplay/ProtectedDisplay';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ProtectedDisplay
+      <h1> hi </h1>
+      <StarterKitSlide title="Workshop 101" subtitle="JOIN US FOR">
+        <p>This is your starter kit. Follow the instructions to set up your project.</p>
+      </StarterKitSlide>
+      {/* <ProtectedDisplay
         allowedRoles={['hacker', 'admin']}
         failRedirectRoute="/login"
-      >
+      > */}
         {children}
-      </ProtectedDisplay>
+      {/* </ProtectedDisplay> */}
     </SessionProvider>
   );
 }
