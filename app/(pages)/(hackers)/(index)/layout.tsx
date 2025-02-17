6219e8b579f02497a8602ba918acb3b12dcf8521
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-17 15:27:54 -0800

    Created files

--- app/(pages)/(hackers)/(index)/layout.tsx
+++ app/(pages)/(hackers)/(index)/layout.tsx
@@ -1,16 +1,26 @@
-import { SessionProvider } from 'next-auth/react';
+// import { SessionProvider } from 'next-auth/react';
+import WorkshopSlides from './starter-kit/_components/FindATeam';
 
-import ProtectedDisplay from '../../_components/ProtectedDisplay/ProtectedDisplay';
+// import ProtectedDisplay from '../../_components/ProtectedDisplay/ProtectedDisplay';
+import StarterKitSlide from './starter-kit/_components/StarterKitSlide';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout(/* { children }: { children: React.ReactNode } */) {
   return (
-    <SessionProvider>
-      <ProtectedDisplay
-        allowedRoles={['hacker', 'admin']}
-        failRedirectRoute="/login"
-      >
-        {children}
-      </ProtectedDisplay>
-    </SessionProvider>
+    <StarterKitSlide
+      subtitle="HERE'S A RECAP OF THE WORKSHOP"
+      title="In case you missed it..."
+    >
+      <WorkshopSlides />
+    </StarterKitSlide>
+
+    // <SessionProvider>
+
+    //   {/* <ProtectedDisplay
+    //     allowedRoles={['hacker', 'admin']}
+    //     failRedirectRoute="/login"
+    //   >
+    //     {children}
+    //   </ProtectedDisplay> */}
+    // </SessionProvider>
   );
 }
