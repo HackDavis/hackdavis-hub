07b36b831427554d371d2aefb808fdaddd6a36f4
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-17 15:30:08 -0800

    Created FindATeam component

---
+++ app/(pages)/(hackers)/(index)/starter-kit/_components/FindATeam.tsx
@@ -0,0 +1,16 @@
+// import { Card, CardContent } from '@globals/components/ui/card';
+// interface WorkshopSlidesProps {
+//   subtitle: string;
+//   title: string;
+//   children: React.ReactNode;
+// }
+
+import styles from './FindATeam.module.scss';
+// import Image from 'next/image';
+
+export default function FindATeam() {
+  return (
+    // <Card className="tw-h-fit tw-p-4 tw-bg-transparent">
+    <div className={styles.Container}></div>
+  );
+}
