74a85468054cf9d53f309ef76b92d599827ef9ba
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-16 23:07:11 -0800

    Updated slides iframe

--- app/(pages)/(hackers)/(index)/starter-kit/_components/WorkshopSlides.tsx
+++ app/(pages)/(hackers)/(index)/starter-kit/_components/WorkshopSlides.tsx
@@ -1,20 +1,16 @@
-import { Card, CardContent } from '@globals/components/ui/card';
-interface WorkshopSlidesProps {
-  subtitle: string;
-  title: string;
-  children: React.ReactNode;
-}
+// import { Card, CardContent } from '@globals/components/ui/card';
+// interface WorkshopSlidesProps {
+//   subtitle: string;
+//   title: string;
+//   children: React.ReactNode;
+// }
 
 import styles from './WorkshopSlides.module.scss';
 import Image from 'next/image';
 import Garage from 'public/hackers/garage.svg';
-import ClosedGarage from 'public/hackers/closed_garage.svg';
+// import ClosedGarage from 'public/hackers/closed_garage.svg';
 
-export default function WorkshopSlides({
-  title,
-  subtitle,
-  children,
-}: WorkshopSlidesProps) {
+export default function WorkshopSlides() {
   return (
     // <Card className="tw-h-fit tw-p-4 tw-bg-transparent">
     <div className={styles.GarageContainer}>
@@ -28,9 +24,15 @@ export default function WorkshopSlides({
         <div className={styles.Overlay}>
           {/* <p className={styles.Text}>insert google slides here</p> */}
           <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vRwPegDb3tMoSINsOSQiDCdoC35qx6gojknjaKE1QyyFwjPVnGUAGB5jKtbsXT-F1paps4rOcr7gjWg/embed?start=false&loop=false&delayms=3000"
+            // frameBorder="0"
+            // width="960"
+            // height="569"
             className={styles.Slides}
-            src="https://docs.google.com/presentation/d/1Ns6j-0nFxoo3QZXAfNKe6f0Rbq79dkE5nzh1LFtkhsE/edit?usp=sharing"
-          />
+            // allowfullscreen="true"
+            // mozallowfullscreen="true"
+            // webkitallowfullscreen="true"
+          ></iframe>
         </div>
       </div>
     </div>
