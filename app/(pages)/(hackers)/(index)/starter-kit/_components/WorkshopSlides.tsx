e239f39eefa327d824af38704a8cbf12378db818
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-16 22:58:25 -0800

    Update WorkshopSlides.tsx

--- app/(pages)/(hackers)/(index)/starter-kit/_components/WorkshopSlides.tsx
+++ app/(pages)/(hackers)/(index)/starter-kit/_components/WorkshopSlides.tsx
@@ -7,8 +7,8 @@ interface WorkshopSlidesProps {
 
 import styles from './WorkshopSlides.module.scss';
 import Image from 'next/image';
-import Garage from 'public/hackers/garage.svg'
-import ClosedGarage from 'public/hackers/closed_garage.svg'
+import Garage from 'public/hackers/garage.svg';
+import ClosedGarage from 'public/hackers/closed_garage.svg';
 
 export default function WorkshopSlides({
   title,
@@ -18,18 +18,21 @@ export default function WorkshopSlides({
   return (
     // <Card className="tw-h-fit tw-p-4 tw-bg-transparent">
     <div className={styles.GarageContainer}>
-        <div className={styles.Garage}>
-            <Image className={styles.Garage}
-                src={Garage}
-                alt='HackDavis Mascots playing instruments in a garage'
-            />
+      <div className={styles.Garage}>
+        <Image
+          className={styles.Garage}
+          src={Garage}
+          alt="HackDavis Mascots playing instruments in a garage"
+        />
 
-            <div className={styles.Overlay}>
-                {/* <p className={styles.Text}>insert google slides here</p> */}
-                <iframe className={styles.Slides} src="https://docs.google.com/presentation/d/1Ns6j-0nFxoo3QZXAfNKe6f0Rbq79dkE5nzh1LFtkhsE/edit?usp=sharing"/>
-            </div>
-
+        <div className={styles.Overlay}>
+          {/* <p className={styles.Text}>insert google slides here</p> */}
+          <iframe
+            className={styles.Slides}
+            src="https://docs.google.com/presentation/d/1Ns6j-0nFxoo3QZXAfNKe6f0Rbq79dkE5nzh1LFtkhsE/edit?usp=sharing"
+          />
         </div>
+      </div>
     </div>
   );
 }
