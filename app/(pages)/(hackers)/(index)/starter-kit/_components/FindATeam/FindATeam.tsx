01a3becda59ff0352f7e39749ab21c9c2205001c
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-17 16:12:30 -0800

    Added const + styled questions box

--- app/(pages)/(hackers)/(index)/starter-kit/_components/FindATeam/FindATeam.tsx
+++ app/(pages)/(hackers)/(index)/starter-kit/_components/FindATeam/FindATeam.tsx
@@ -9,16 +9,40 @@ import StarterKitSlide from '../StarterKitSlide';
 import styles from './FindATeam.module.scss';
 // import Image from 'next/image';
 
+const questions = [
+  {
+    text: 'IS THIS PERSON PASSIONATE ABOUT THE SAME TRACK(S)?',
+    color: styles.qboxBlue,
+  },
+  {
+    text: 'DOES THIS PERSON’S SKILLS COMPLIMENT MINE?',
+    color: styles.qboxYellow,
+  },
+  {
+    text: 'CAN I SEE MYSELF WORKING WITH THEM FOR 24 HOURS?',
+    color: styles.qboxGreen,
+  },
+];
+
 export default function FindATeam() {
   return (
     <div>
-      {' '}
-      {/* Add a wrapper div */}
       <StarterKitSlide
-        subtitle="GUIDING THE QUESTIONS"
+        subtitle="GUIDING QUESTIONS"
         title="Finding the right fit"
       >
-        <div className={styles.Container}></div>
+        <div className={styles.Container}>
+          <div className={styles.questions}>
+            <h3>Ask yourself questions like...</h3>
+            <div className={styles.questionBoxes}>
+              {questions.map((q, index) => (
+                <div key={index} className={`${styles.qbox} ${q.color}`}>
+                  <p>{q.text}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </StarterKitSlide>
     </div>
   );
