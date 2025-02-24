7048af4025e11f0778e196e95622d3e16274df37
reehals <sunny.reehal@gmail.com>
2025-02-24 01:04:50 -0800

    Mobile waterfall almost done, needs xs sizing

--- app/(pages)/(hackers)/(index)/_components/HackerHub/HackerHub.tsx
+++ app/(pages)/(hackers)/(index)/_components/HackerHub/HackerHub.tsx
@@ -4,6 +4,7 @@ export default function HackerHub() {
   return (
     <main>
       <div>
+        {/* Remove when adding vinyl or other components on top, just to see the whole flowers component */}
         <div className="h-[400px]">Spacer</div>
         <Waterfall />
       </div>
