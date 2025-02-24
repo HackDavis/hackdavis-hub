d0b9920bf90793780de7f551a1edbdebc4516938
reehals <sunny.reehal@gmail.com>
2025-02-24 14:34:25 -0800

    Update Waterfall.tsx

--- app/(pages)/(hackers)/(index)/_components/Waterfall/Waterfall.tsx
+++ app/(pages)/(hackers)/(index)/_components/Waterfall/Waterfall.tsx
@@ -16,65 +16,65 @@ import WaterfallImage from '@public/Waterfall/waterfall.svg';
 
 export default function Waterfall() {
   return (
-    <div className="relative flex w-0 lg:w-full overflow-x-clip flex-col items-center justify-evenly lg:bg-grass-background-darker lg:w-full lg:h-[calc(100vw*600/1440)] z-10">
+    <div className="relative flex w-0 md:w-full overflow-x-clip flex-col items-center justify-evenly md:bg-grass-background-darker md:w-full md:h-[calc(100vw*600/1440)] z-10">
       {/* Top div with WaterfallGrass */}
-      <div className="absolute top-[-26.5%] w-0 lg:w-[100%] aspect-[1440/463]">
+      <div className="absolute top-[-26.5%] w-0 md:w-[100%] aspect-[1440/463]">
         <Image src={WaterfallGrass} alt="Waterfall Grass" className="w-full" />
       </div>
 
       {/* Bottom div with three sections in a row */}
-      <div className="w-0 h-0 lg:h-auto lg:w-full flex justify-between items-start z-10">
+      <div className="w-0 h-0 md:h-auto md:w-full flex justify-between items-start z-10">
         {/* Leftmost item */}
         <div className="flex flex-col space-y-2 flex-1">
-          <div className="relative z-30 aspect-[245/271] w-0 lg:w-[calc(100vw*245/1440)] left-[calc(100vw*150/1440)] translate-y-[calc(-1*100vw*40/1440)]">
+          <div className="relative z-30 aspect-[245/271] w-0 md:w-[calc(100vw*245/1440)] left-[calc(100vw*150/1440)] translate-y-[calc(-1*100vw*40/1440)]">
             <Image src={Bunny} alt="Bunny" className="w-full" />
           </div>
-          <div className="relative z-30 aspect-[464/286] w-0 lg:w-[calc(100vw*464/1440)] translate-y-[calc(-1*100vw*80/1440)]">
+          <div className="relative z-30 aspect-[464/286] w-0 md:w-[calc(100vw*464/1440)] translate-y-[calc(-1*100vw*80/1440)]">
             <Image
               src={BunnyPlatform}
               alt="Bunny Platform"
               className="w-full"
             />
           </div>
-          <div className="relative z-40 aspect-[169/222] w-0 lg:w-[calc(100vw*169/1440)] left-[calc(100vw*239/1440)] translate-y-[calc(-1*100vw*360/1440)]">
+          <div className="relative z-40 aspect-[169/222] w-0 md:w-[calc(100vw*169/1440)] left-[calc(100vw*239/1440)] translate-y-[calc(-1*100vw*360/1440)]">
             <Image src={Cow} alt="Cow" className="w-full" />
           </div>
-          <div className="relative z-30 aspect-[509/210] w-0 lg:w-[calc(100vw*509/1440)] translate-y-[calc(-1*100vw*425/1440)]">
+          <div className="relative z-30 aspect-[509/210] w-0 md:w-[calc(100vw*509/1440)] translate-y-[calc(-1*100vw*425/1440)]">
             <Image src={CowPlatform} alt="Cow Platform" className="w-full" />
           </div>
         </div>
 
         {/* Middle Item */}
-        <div className="relative z-10 aspect-[206/700] w-0 lg:w-[calc(100vw*206/1440)] translate-y-[calc(-1*100vw*5/1440)] 2xl:translate-y-[calc(-1*100vw*5/1440)]">
+        <div className="relative z-10 aspect-[206/700] w-0 md:w-[calc(100vw*206/1440)] translate-y-[calc(-1*100vw*5/1440)] 2xl:translate-y-[calc(-1*100vw*5/1440)]">
           <Image src={WaterfallImage} className="w-full" alt="Waterfall" />
         </div>
 
         {/* Rightmost item */}
         <div className="flex flex-col items-end space-y-2 flex-1 z-10 top-[calc(100vw*240/1440)]">
-          <div className="relative z-10 aspect-[227/230] w-0 lg:w-[calc(100vw*227/1440)] right-[calc(100vw*154/1440)] translate-y-[calc(100vw*157/1440)]">
+          <div className="relative z-10 aspect-[227/230] w-0 md:w-[calc(100vw*227/1440)] right-[calc(100vw*154/1440)] translate-y-[calc(100vw*157/1440)]">
             <Image
               src={KeyboardDucky}
               alt="Keyboard Ducky"
               className="w-full"
             />
           </div>
-          <div className="relative z-20 aspect-[480/153] w-0 lg:w-[calc(100vw*480/1440)] translate-y-[calc(100vw*130/1440)]">
+          <div className="relative z-20 aspect-[480/153] w-0 md:w-[calc(100vw*480/1440)] translate-y-[calc(100vw*130/1440)]">
             <Image src={DuckPlatform} alt="Duck Platform" className="w-full" />
           </div>
-          <div className="relative z-40 aspect-[260/152] w-0 lg:w-[calc(100vw*260/1440)] right-[calc(100vw*190/1440)] translate-y-[calc(-1*100vw*15/1440)]">
+          <div className="relative z-40 aspect-[260/152] w-0 md:w-[calc(100vw*260/1440)] right-[calc(100vw*190/1440)] translate-y-[calc(-1*100vw*15/1440)]">
             <Image
               src={SleepingFroggy}
               alt="Sleeping Froggy"
               className="w-full"
             />
           </div>
-          <div className="relative z-30 asect-[506/293] w-0 lg:w-[calc(100vw*506/1440)] translate-y-[calc(-1*100vw*100/1440)]">
+          <div className="relative z-30 asect-[506/293] w-0 md:w-[calc(100vw*506/1440)] translate-y-[calc(-1*100vw*100/1440)]">
             <Image src={FrogPlatform} alt="Frog Platform" className="w-full" />
           </div>
         </div>
       </div>
 
-      <div className="relative z-20 aspect-[536/220] w-0 lg:w-[calc(100vw*536/1440)] lg:translate-y-[calc(-1*100vw*560/1440)]">
+      <div className="relative z-20 aspect-[536/220] w-0 md:w-[calc(100vw*536/1440)] md:translate-y-[calc(-1*100vw*560/1440)]">
         <Image
           src={WaterfallSplash}
           alt="Waterfall Splash"
@@ -82,12 +82,12 @@ export default function Waterfall() {
         />
       </div>
 
-      <div className="relative z-10 aspect-[134/164] w-0 lg:w-[calc(100vw*134/1440)] left-[calc(100vw*300/1440)] translate-y-[calc(-1*100vw*1150/1440)]">
+      <div className="relative z-10 aspect-[134/164] w-0 md:w-[calc(100vw*134/1440)] left-[calc(100vw*300/1440)] translate-y-[calc(-1*100vw*1150/1440)]">
         <Image src={MusicNotes} alt="Music Notes" className="w-full" />
       </div>
 
       {/* Temporarily removed */}
-      {/* <div className="relative z-10 aspect-[227/230] w-[calc(100vw*227/375)] right-[calc(100vw*40/375)] lg:w-0">
+      {/* <div className="relative z-10 aspect-[227/230] w-[calc(100vw*227/375)] right-[calc(100vw*40/375)] md:w-0">
         <Image src={KeyboardDucky} alt="Keyboard Ducky" className="w-full" />
       </div> */}
     </div>
