0030b5252c0a4fe50186693fec6b8851180ba0b0
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-02-25 21:47:31 -0800

    moved folders around

--- app/(pages)/(hackers)/(index)/starter-kit/page.tsx
+++ app/(pages)/(hackers)/(index)/starter-kit/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ParentCarousel } from '../../_components/StarterKitSlide/ParentCarousel/ParentCarousel';
+import { ParentCarousel } from '../../_components/StarterKit/ParentCarousel';
 
 export default function Page() {
   return (
