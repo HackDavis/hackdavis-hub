971d5b50b76d60c660ca1d17c37d3302b175686b
michelleyeoh <michellew.yeoh@gmail.com>
2025-03-01 09:21:55 -0800

    added to ideate slide

--- app/(pages)/(hackers)/_components/StarterKitStages/Ideate.tsx
+++ app/(pages)/(hackers)/_components/StarterKitStages/Ideate.tsx
@@ -1,3 +1,6 @@
+import Mentor from '../StarterKit/Mentor';
+import StarterKitSlide from '../StarterKit/StarterKitSlide';
+
 export default function Ideate() {
   return (
     <div className="flex flex-col gap-8 md:gap-12 xl:gap-16">
@@ -6,6 +9,12 @@ export default function Ideate() {
 
       <h1>Stage 3 (Replace this w the components)</h1>
       {/* ensure the content, esp text, within each component horizontally lines up with the content inside other components */}
+      <StarterKitSlide
+        title="Talk with a Mentor"
+        subtitle="STILL FEELIN STUCK?"
+      >
+        <Mentor />
+      </StarterKitSlide>
     </div>
   );
 }
