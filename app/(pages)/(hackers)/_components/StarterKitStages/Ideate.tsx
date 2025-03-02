f373694cae0d5712e5f735c59f372f5f4c0f39e4
michelleyeoh <74385095+michelleyeoh@users.noreply.github.com>
2025-03-02 03:50:00 -0800

    201 starter kit brainstorm and youre ready (#227)
    * rough outline
    
    * added stars and finalized components
    
    * Update Resources.tsx
    
    * quick fix
    
    * removed youreReady
    
    * fixed rounded bottom edge
    
    ---------
    
    Co-authored-by: Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>

--- app/(pages)/(hackers)/_components/StarterKitStages/Ideate.tsx
+++ app/(pages)/(hackers)/_components/StarterKitStages/Ideate.tsx
@@ -1,11 +1,12 @@
+import Brainstorm from '../StarterKit/Brainstorm';
+import StarterKitSlide from '../StarterKit/StarterKitSlide';
+
 export default function Ideate() {
   return (
     <div className="flex flex-col gap-8 md:gap-12 xl:gap-16">
-      {/* The children here should be StarterKitSlide components that wrap around the component content you created */}
-      {/* Refer ./LetsBegin.tsx */}
-
-      <h1>Stage 3 (Replace this w the components)</h1>
-      {/* ensure the content, esp text, within each component horizontally lines up with the content inside other components */}
+      <StarterKitSlide title="Brainstorm" subtitle="IT’S TIME TO">
+        <Brainstorm />
+      </StarterKitSlide>
     </div>
   );
 }
