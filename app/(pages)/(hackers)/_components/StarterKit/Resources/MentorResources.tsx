d62071d866b83f5dee23b82e10d033e29dd55bc0
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-03-02 01:55:01 -0800

    mentor resources responsive

--- app/(pages)/(hackers)/_components/StarterKit/Resources/MentorResources.tsx
+++ app/(pages)/(hackers)/_components/StarterKit/Resources/MentorResources.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import bunny_duck_mentor from '@public/starterKIt/rabbit_duck_mentor.svg';
 export default function MentorResources() {
   return (
-    <main className="relative p-12 pb-0 h-[300px] md:h-[400px] lg:h-[560px] flex flex-col justify-end items-center bg-[#005271] rounded-xl overflow-hidden">
+    <main className="relative pt-[10%] w-full aspect-[2] flex flex-col justify-end items-center bg-[#005271] rounded-xl overflow-hidden">
       <div className="relative w-full h-full">
         <div className="relative w-full h-full flex items-end justify-end">
           <Image
@@ -12,16 +12,16 @@ export default function MentorResources() {
             fill
             priority
           />
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute bottom-[10%] md:bottom-[15%] left-1/2 transform -translate-x-1/2 font-metropolis font-bold text-xs sm:text-lg md:text-2xl lg:text-4xl tracking-[0.02em] underline text-white z-100 hover:opacity-80 transition-opacity"
+          >
+            Click Here
+          </a>
         </div>
-        {/* NEED TO ADD LINK */}
-        <a
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="absolute bottom-3 xs:bottom-10 sm:bottom-16 left-1/2 transform -translate-x-1/2 font-metropolis font-bold text-base sm:text-xl md:text-2xl lg:text-[36px] leading-[1.2] sm:leading-[1.3] md:leading-[1.4] lg:leading-[40px] tracking-[0.02em] underline text-white z-100 hover:opacity-80 transition-opacity"
-        >
-          Click Here
-        </a>
+        {/* TODO: NEED TO ADD LINK */}
       </div>
     </main>
   );
