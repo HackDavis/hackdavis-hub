7f62817f649d1cf223963532a90c1ecee311a308
Sandeep Reehal <126985041+ReehalS@users.noreply.github.com>
2025-03-02 04:44:31 -0800

    202 previous winning hacks (#226)
    * Previous winning hacks complete
    
    * Add link to DevPost
    
    * Update Ideate.tsx
    
    * moved into a component
    
    * fixed spacing
    
    ---------
    
    Co-authored-by: Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>

--- app/(pages)/(hackers)/_components/StarterKitStages/Ideate.tsx
+++ app/(pages)/(hackers)/_components/StarterKitStages/Ideate.tsx
@@ -1,11 +1,18 @@
-import Brainstorm from '../StarterKit/Brainstorm';
 import StarterKitSlide from '../StarterKit/StarterKitSlide';
+import PreviousHacks from '../StarterKit/PreviousHacks';
+import Brainstorm from '../StarterKit/Brainstorm';
 
 export default function Ideate() {
   return (
     <div className="flex flex-col gap-8 md:gap-12 xl:gap-16">
       <StarterKitSlide title="Brainstorm" subtitle="IT’S TIME TO">
         <Brainstorm />
+      </StarterKitSlide>
+      <StarterKitSlide
+        title="Previous Winning Hacks"
+        subtitle="YOU CAN REFERENCE"
+      >
+        <PreviousHacks />
       </StarterKitSlide>
     </div>
   );
