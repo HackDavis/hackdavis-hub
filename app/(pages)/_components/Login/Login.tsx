a0a0dd90b73060ec8c4c5f264a96a44b970b3cdd
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-02 13:56:44 -0800

    Styled clouds, grass, logo

--- app/(pages)/_components/Login/Login.tsx
+++ app/(pages)/_components/Login/Login.tsx
@@ -1,11 +1,35 @@
-// import { auth } from '@/auth';
-// import { redirect } from 'next/navigation';
 import styles from './Login.module.scss';
+import Image from 'next/image';
+// import MusicAnimals from 'public/hackers/mvp/MusicAnimals.svg';
+import Clouds from 'public/hackers/mvp/Clouds.svg';
+import BottomGrass from 'public/hackers/mvp/BottomGrass.svg';
+import Logo from 'public/hackers/mvp/HDLogo.svg';
 
 export default async function RegistrationLogin({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      {/* <div className={styles.imageWrapper}>
+        <Image
+          src={MusicAnimals}
+          alt="animals peeping"
+          fill
+          //   style={{ objectFit: 'contain' }}
+          className={styles.animals}
+        />
+      </div> */}
+
+      {/* Overlayed Content */}
+      <div className={styles.overlayContent}>{children}</div>
+
+      <div className={styles.scenery}>
+        <Image src={Logo} alt="hackdavis logo" className={styles.logo} />
+        <Image src={Clouds} alt="clouds" className={styles.clouds} />
+        <Image src={BottomGrass} alt="grass" className={styles.grass} />
+      </div>
+    </div>
+  );
 }
