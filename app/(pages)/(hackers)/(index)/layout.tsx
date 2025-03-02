a74df350f43481517a3d0f6fe176be9a10faa478
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2025-03-01 17:44:13 -0800

    199 starter kit find the right fit (#207)
    * Added FindATeam component
    
    * Updated component naming issue
    
    * Made it viewable locally (will disregard these changes)
    
    * Removed discord part
    
    * Added Animals + made mobile
    
    * moved files
    
    * disregard
    
    * Added animals as image
    
    * Fixed overlap between animals and text bubbles
    
    * Updated padding + image
    
    * Responsiveness
    
    * Added wrapper to the image
    
    * Updated font sizing
    
    * fixed styles
    
    ---------
    
    Co-authored-by: Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>

--- app/(pages)/(hackers)/(index)/layout.tsx
+++ app/(pages)/(hackers)/(index)/layout.tsx
@@ -1,9 +1,13 @@
 import { SessionProvider } from 'next-auth/react';
-
+// import StarterKitSlide from '../_components/StarterKit/StarterKitSlide';
+// import FindTheRightFit from '../_components/StarterKit/FindTheRightFit/FindTheRightFit';
 import ProtectedDisplay from '../../_components/ProtectedDisplay/ProtectedDisplay';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+    //   <StarterKitSlide>
+    //     <FindTheRightFit />
+    //   </StarterKitSlide>
     <SessionProvider>
       <ProtectedDisplay
         allowedRoles={['hacker', 'admin']}
--- app/(pages)/(hackers)/_components/StarterKitStages/FindATeam.tsx
+++ app/(pages)/(hackers)/_components/StarterKitStages/FindATeam.tsx
@@ -1,5 +1,6 @@
 import StarterKitSlide from '../StarterKit/StarterKitSlide';
-import EventPosting from '../StarterKit/EventPosting';
+import EventPosting from '../StarterKit/EventPosting/EventPosting';
+import FindTheRightFit from '../StarterKit/FindTheRightFit/FindTheRightFit';
 
 export default function FindATeam() {
   return (
@@ -12,6 +13,7 @@ export default function FindATeam() {
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip"
         ></EventPosting>
       </StarterKitSlide>
+      <FindTheRightFit />
     </div>
   );
 }
