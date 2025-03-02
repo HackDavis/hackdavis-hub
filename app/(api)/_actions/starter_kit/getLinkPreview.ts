c8ca52c182f95e43617cb855891c62bf0cee6a3b
Win Cheng <winzamark12@gmail.com>
2025-03-01 16:50:43 -0800

    type casting the results to match

--- app/(api)/_actions/starter_kit/getLinkPreview.ts
+++ app/(api)/_actions/starter_kit/getLinkPreview.ts
@@ -1,17 +1,30 @@
 'use server';
 
 import { getLinkPreview } from 'link-preview-js';
+interface LinkPreviewResult {
+  url: string;
+  title?: string;
+  siteName?: string;
+  description?: string;
+  mediaType?: string;
+  contentType?: string;
+  images?: string[];
+  videos?: any[];
+  favicons?: string[];
+  charset?: string | null;
+}
 
 export async function fetchLinkPreview(url: string) {
   try {
-    const data = await getLinkPreview(url, {
+    const data = (await getLinkPreview(url, {
       imagesPropertyType: 'og',
       headers: {
         'user-agent': 'googlebot',
         'Accept-Language': 'en-US',
       },
+      followRedirects: 'follow',
       timeout: 5000,
-    });
+    })) as LinkPreviewResult;
 
     return {
       ok: true,
--- next.config.js
+++ next.config.js
@@ -9,8 +9,14 @@ const nextConfig = {
       'youtube.com',
       'img.youtube.com',
       'i.ytimg.com',
+      'youtu.be',
       'www.figma.com',
       'static.figma.com',
+      'ideo.com',
+      'www.ideo.com',
+      'images.ctfassets.net', // Common CDN for content
+      'cdn.sanity.io', // Common CDN for content
+      'res.cloudinary.com', // Common CDN for content
       // Add any other domains you might need
     ],
   },
