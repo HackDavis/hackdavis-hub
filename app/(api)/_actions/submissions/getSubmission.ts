00759b4ebb55291f6a9d7a04c09d74e8963631f9
Daniel Kim <hyunkim03@gmail.com>
2025-03-02 18:04:03 -0800

    Serializing working

--- app/(api)/_actions/submissions/getSubmission.ts
+++ app/(api)/_actions/submissions/getSubmission.ts
@@ -6,19 +6,20 @@ import {
   GetJudgeSubmissions,
 } from '@datalib/submissions/getSubmissions';
 import parseAndReplace from '@utils/request/parseAndReplace';
+import { serializeMongoData } from '@utils/serialize/serialization';
 
 export async function getSubmission(judge_id: string, team_id: string) {
   const submission = await GetSubmission(judge_id, team_id);
-  return submission;
+  return serializeMongoData(submission);
 }
 
 export async function getManySubmissions(query: object = {}) {
   const newQuery = await parseAndReplace(query);
   const submissions = await GetManySubmissions(newQuery);
-  return submissions;
+  return serializeMongoData(submissions);
 }
 
 export async function getJudgeSubmissions(judge_id: string) {
   const submissions = await GetJudgeSubmissions(judge_id);
-  return submissions;
+  return serializeMongoData(submissions);
 }
