737673174ca5363e54fff1d2cc4b4b3388bb1a8e
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-03-15 02:35:14 -0700

    added mirations and seeding for in-memory db

--- tests/connection.db.spec.ts
+++ tests/connection.db.spec.ts
@@ -3,9 +3,38 @@ import { expect } from '@playwright/test';
 
 test('testing connection to in-memory database', async ({
   mongoServer,
+  mongoClient,
   db,
 }) => {
   expect(mongoServer.getUri()).toBe;
   expect((global as any).__TEST_CLIENT__).toBeTruthy();
+  expect((mongoClient as any).s.url).toBe(mongoServer.getUri());
   expect(db.databaseName).toBe('test');
+});
+
+test('testing migration', async ({ db }) => {
+  const dbCollections = (await db.listCollections().toArray()).map(
+    (c) => c.name
+  );
+  expect(dbCollections).toContain('events');
+  expect(dbCollections).toContain('submissions');
+  expect(dbCollections).toContain('teams');
+  expect(dbCollections).toContain('users');
+  expect(dbCollections).toContain('userToEvents');
+  expect(dbCollections).toContain('changelog');
+});
+
+test('generating collections with 10 documents', async ({ db, seedData }) => {
+  await seedData('all', 10);
+
+  const dbCollections = (await db.listCollections().toArray())
+    .map((c) => c.name)
+    .filter((c) => c !== 'changelog' && c !== 'userToEvents');
+
+  const findPromises = dbCollections.map(async (collection) => {
+    const docs = await db.collection(collection).find({}).toArray();
+    expect(docs.length).toBe(collection === 'users' ? 21 : 10);
+  });
+
+  await Promise.all(findPromises);
 });
--- tests/test.ts
+++ tests/test.ts
@@ -1,12 +1,15 @@
 import { test as base } from '@playwright/test';
 import { MongoClient, Db } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import migrate from 'migrate-mongo';
+import generateData from '../scripts/generateData.mjs';
 
 // https://playwright.dev/docs/test-fixtures
 type MongoFixture = {
   mongoServer: MongoMemoryServer;
   mongoClient: MongoClient;
   db: Db;
+  seedData: (collectionName: string, numDocs: number) => Promise<void>;
 };
 
 export const test = base.extend<MongoFixture>({
@@ -30,11 +33,48 @@ export const test = base.extend<MongoFixture>({
     await testClient.close();
   },
 
-  db: async (
-    { mongoClient }: { mongoClient: MongoClient },
-    use: (db: Db) => Promise<void>
-  ) => {
+  db: async ({ mongoServer, mongoClient }, use) => {
     const db = mongoClient.db();
+
+    const migrationConfig = {
+      mongodb: { url: mongoServer.getUri() },
+      migrationsDir: 'migrations',
+      changelogCollectionName: 'changelog',
+      migrationFileExtension: '.mjs',
+      useFileHash: false,
+      moduleSystem: 'esm',
+    };
+    await migrate.config.set(migrationConfig);
+    await migrate.up(db, mongoClient);
+
     await use(db);
+  },
+
+  seedData: async ({ db }, use) => {
+    const seedData = async (collectionName: string, numDocs: number) => {
+      const collection = collectionName === 'admin' ? 'users' : collectionName;
+      if (collection !== 'all') {
+        const data = generateData(collection, numDocs);
+        if (data.length !== 0) await db.collection(collection).insertMany(data);
+        return;
+      }
+
+      const allCollections = await db.listCollections().toArray();
+      allCollections.push({
+        name: 'admin',
+      });
+
+      const insertPromises = allCollections.map(async (collection) => {
+        const data = generateData(collection.name, numDocs);
+        if (data.length === 0) return;
+        await db
+          .collection(collection.name === 'admin' ? 'users' : collection.name)
+          .insertMany(data);
+      });
+
+      await Promise.all(insertPromises);
+    };
+
+    await use(seedData);
   },
 });
