2d9e4a191d6ac872bf5be19c43a7e1f04f062167
Austin Shih <austinhshih@gmail.com>
2025-03-26 18:07:30 -0700

    Persistent Submissions

--- app/(api)/_actions/submissions/updateSubmission.ts
+++ app/(api)/_actions/submissions/updateSubmission.ts
@@ -1,27 +1,17 @@
 'use server';
-import FormToJSON from '@utils/form/FormToJSON';
 import { UpdateSubmission } from '@datalib/submissions/updateSubmission';
+import Submission from '@typeDefs/submission';
 
 export default async function updateSubmission(
-  prevState: any,
-  formData: FormData
+  judge_id: string,
+  team_id: string,
+  body: Submission
 ) {
-  const { judge_id, team_id, comments, ...rest } = FormToJSON(formData);
-  const keys = Object.keys(rest);
-  const scores = keys.slice(0, 5).map((key: string) => +rest[key]);
-  const correlations = keys.slice(5).map((key: string) => ({
-    track: Buffer.from(key, 'ascii').toString('utf-8'),
-    score: +rest[key],
-  }));
+  const { _id: _, judge_id: __, team_id: ___, ...rest } = body;
 
-  const updateBody = {
-    $set: {
-      scores,
-      correlations,
-      comments,
-    },
-  };
+  const updateRes = await UpdateSubmission(judge_id, team_id, {
+    $set: rest,
+  });
 
-  const updateRes = await UpdateSubmission(judge_id, team_id, updateBody);
   return updateRes;
 }
--- app/(pages)/_components/ScoringForm/ScoringForm.tsx
+++ app/(pages)/_components/ScoringForm/ScoringForm.tsx
@@ -1,14 +1,25 @@
 import styles from './ScoringForm.module.scss';
 import RadioSelect from '@components/RadioSelect/RadioSelect';
 import tracks from '@data/tracks';
+import Submission from '@typeDefs/submission';
 import { useRef, useState } from 'react';
+import {
+  UpdateSubmission,
+  FlattenScores,
+} from 'app/(pages)/judges/(app)/score/[team-id]/FormParser';
+import updateSubmission from '@actions/submissions/updateSubmission';
 
 interface ScoringFormProps {
-  submission_id: string;
+  submission: Submission;
 }
 
+const overallScoringCategory = [
+  { displayName: 'Social Good', name: 'social_good' },
+  { displayName: 'Creativity', name: 'creativity' },
+  { displayName: 'Presentation', name: 'presentation' },
+];
+
 const judgingCategories = [
-  'Overall Scoring',
   'Best Usage of MongoDB',
   'Best Social Hack',
   'Best Beginner Hack',
@@ -17,27 +28,40 @@ const judgingCategories = [
 
 const SEP = '::';
 
-export default function ScoringForm({ submission_id: _ }: ScoringFormProps) {
-  const dataRef = useRef({});
-  const [comment, setComment] = useState('');
+export default function ScoringForm({ submission }: ScoringFormProps) {
+  const dynamicQuestionsRef = useRef(FlattenScores(submission.scores));
+  const [baseQuestions, setBaseQuestions] = useState({
+    comments: submission.comments,
+    social_good: submission.social_good,
+    creativity: submission.creativity,
+    presentation: submission.presentation,
+  } as { [key: string]: any });
 
-  const setData = (
-    key: string,
-    value: { track: string; question: string; value: any }
-  ) => {
-    dataRef.current = {
-      ...dataRef.current,
+  const setData = (key: string, value: number) => {
+    dynamicQuestionsRef.current = {
+      ...dynamicQuestionsRef.current,
       [key]: value,
     };
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // Correct way to prevent form submission
-    console.log({
-      comment,
-      data: dataRef.current,
-    });
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const updatedSubmission = UpdateSubmission(
+      submission,
+      baseQuestions,
+      dynamicQuestionsRef.current
+    );
+
+    const updateRes = await updateSubmission(
+      submission.judge_id,
+      submission.team_id,
+      updatedSubmission
+    );
+
+    console.log(updateRes);
   };
+
   return (
     <form onSubmit={onSubmit} className={styles.form}>
       <div className={styles.comments_container}>
@@ -46,12 +70,28 @@ export default function ScoringForm({ submission_id: _ }: ScoringFormProps) {
         <textarea
           className={styles.comment_box}
           name="comments"
-          value={comment}
+          value={baseQuestions.comments}
           placeholder={'Notes...'}
           onChange={(event) => {
-            setComment(event.target.value);
+            setBaseQuestions((prev: any) => ({
+              ...prev,
+              comments: event.target.value,
+            }));
           }}
         />
+      </div>
+      <div className={styles.track_container}>
+        <h2 className={styles.category_header}>Overall Scoring</h2>
+        {overallScoringCategory.map(({ displayName, name }) => (
+          <RadioSelect
+            key={`Overall Scoring: ${name}`}
+            question={displayName}
+            onChange={(value) => {
+              setBaseQuestions((prev: any) => ({ ...prev, [name]: value }));
+            }}
+            initValue={baseQuestions[name]}
+          />
+        ))}
       </div>
       {judgingCategories.map((category) => (
         <div key={category} className={styles.track_container}>
@@ -61,12 +101,11 @@ export default function ScoringForm({ submission_id: _ }: ScoringFormProps) {
               key={`${category}: ${question}`}
               question={question}
               onChange={(value) => {
-                setData(`${category}${SEP}${question}`, {
-                  track: category,
-                  question,
-                  value,
-                });
+                setData(`${category}${SEP}${question}`, value);
               }}
+              initValue={
+                dynamicQuestionsRef.current[`${category}${SEP}${question}`]
+              }
             />
           ))}
         </div>
