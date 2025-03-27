cca8c03048b53a34b88766206dcf6ad687b54bec
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-03-26 19:05:26 -0700

    more tests to trigger workflow

--- __tests__/datalib/events.ts
+++ __tests__/datalib/events.ts
@@ -1,5 +1,6 @@
 import { db } from '../../jest.setup';
 import { CreateEvent } from '@datalib/events/createEvent';
+import Event from '@typeDefs/event';
 
 const mockEvent1 = {
   name: 'Test Event 1',
@@ -26,7 +27,6 @@ beforeEach(async () => {
 describe('CREATE: events', () => {
   it('should create an event with valid details successfully', async () => {
     const { ok, body, error } = await CreateEvent(mockEvent1);
-    console.log(`ok: ${ok}, body: ${JSON.stringify(body)}, error: ${error}`);
     expect(ok).toBe(true);
     expect(body).not.toBe(null);
     expect(body).toEqual(mockEvent1);
@@ -43,7 +43,7 @@ describe('CREATE: events', () => {
     delete (tempEvent2 as any).type;
 
     // missing start_time
-    const tempEvent3 = { ...mockEvent2 };
+    const tempEvent3 = { ...mockEvent1 };
     delete (tempEvent3 as any).start_time;
 
     [tempEvent1, tempEvent2, tempEvent3].forEach(async (event) => {
@@ -53,5 +53,69 @@ describe('CREATE: events', () => {
       expect(res.error).not.toBe(null);
       expect(res.error).toBe('Document failed validation');
     });
+  });
+
+  it('should create an event with missing optional fields', async () => {
+    // missing host
+    const tempEvent1 = { ...mockEvent2 };
+    delete (tempEvent1 as Event).host;
+
+    // missing location
+    const tempEvent2 = { ...mockEvent2 };
+    delete (tempEvent2 as Event).location;
+
+    // missing end_time
+    const tempEvent3 = { ...mockEvent2 };
+    delete (tempEvent3 as Event).end_time;
+
+    // missing tags
+    const tempEvent4 = { ...mockEvent2 };
+    delete (tempEvent4 as Event).tags;
+
+    [tempEvent1, tempEvent2, tempEvent3, tempEvent4].forEach(async (event) => {
+      const res = await CreateEvent(event);
+      expect(res.ok).toBe(true);
+      const insertedEvent = res.body as Event;
+      delete insertedEvent._id;
+      expect(res.body).toStrictEqual(insertedEvent);
+      expect(res.error).toBe(null);
+    });
+  });
+
+  // it('should fail to create an event with start_time before April 19', async () => {
+  //   const tempEvent = { ...mockEvent1 };
+  //   tempEvent.start_time = new Date(2000, 1, 1, 0, 0, 0, 0);
+
+  //   const { ok, body, error } = await CreateEvent(tempEvent);
+  //   expect(ok).toBe(false);
+  //   expect(body).toBe(null);
+  //   expect(error).toBe(
+  //     'Failed to create event: start_time must be after 04/18/2025'
+  //   );
+  // });
+
+  // it('should fail to create an event with end_time before start_time', async () => {
+  //   const tempEvent = { ...mockEvent1 };
+  //   tempEvent.start_time = new Date();
+  //   tempEvent.end_time = new Date(2000, 1, 1, 0, 0, 0, 0);
+
+  //   const { ok, body, error } = await CreateEvent(tempEvent);
+  //   expect(ok).toBe(false);
+  //   expect(body).toBe(null);
+  //   expect(error).toBe(
+  //     'Failed to create event: end_time must be after start_time'
+  //   );
+  // });
+
+  it('should fail to create an event with duplicate name', async () => {
+    const { ok, body, error } = await CreateEvent(mockEvent1);
+    expect(ok).toBe(true);
+    expect(body).toEqual(mockEvent1);
+    expect(error).toBe(null);
+
+    const duplicate = await CreateEvent(mockEvent1);
+    expect(duplicate.ok).toBe(false);
+    expect(duplicate.body).toBe(null);
+    expect(duplicate.error).toBe('Duplicate: event already exists.');
   });
 });
