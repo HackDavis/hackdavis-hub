c249a827cda409a3917c61ef23835cd4e00f237b
brandonw504 <brandonw504@outlook.com>
2025-03-28 12:40:35 -0700

    Added an invite email format

--- app/(api)/_actions/invite/sendEmail.ts
+++ app/(api)/_actions/invite/sendEmail.ts
@@ -9,6 +9,7 @@ import {
   NotFoundError,
 } from '@utils/response/Errors';
 import { GetManyUsers } from '@datalib/users/getUser';
+import emailMessage from './emailMessage';
 
 const senderEmail = process.env.SENDER_EMAIL;
 const password = process.env.SENDER_PWD;
@@ -52,92 +53,15 @@ export default async function sendEmail(
       },
     });
 
-    const msg = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Page Title</title>
-    <style>
-    * {
-      box-sizing: border-box;
-      color: white;
-      text-decoration: none;
-    }
-    
-    .container {
-      background-color: #173A52;
-      width: 100%;
-      padding: 48px;
-    }
-    
-    .welcome-text {
-      color: white;
-      font-size: 1.5rem;
-      font-weight: 700;
-      font-family: 'Helvetica';
-    }
-    
-    .name-text {
-      color: #76D6E6;
-      font-family: 'Helvetica';
-    }
-    
-    .make-account {
-      color: white;
-      font-family: 'Helvetica';
-      margin-bottom: 30px;
-    }
-    
-    span {
-      font-weight: 700;
-    }
-    
-    .button {
-      border: none;
-      border-radius: 4px;
-      padding: 12px;
-      background-color: #FFC53D;
-      font-weight: 500;
-      font-family: 'Proxima Nova';
-      font-size: 1.25rem;
-      color: #173a52;
-      cursor: pointer;
-      text-align: center;
-      text-decoration: none;
-      font-family: 'Helvetica';
-    }
-    
-    .bottom-text {
-      color: white;
-      font-family: 'Helvetica';
-    }
-    
-    
-    </style>
-    </head>
-    <body>
-    <div class="container">
-      <h3 class="welcome-text">Reset Password Request for the HackDavis Hub</h3>
-      <p class="make-account">
-        Please reset your password for the HackDavis Hub using the following invite
-        link. Do <span>NOT</span> share this link with anyone. If you didn't request
-        a password reset, you can safely ignore this email.
-      </p>
-      <a class="button" href="${invite_link}">Reset Password</a>
-      <div>
-        <p class="bottom-text">Your reset password link is:</p>
-        <p class="bottom-text">${invite_link}</p>
-      </div>
-     </div>
-    </body>
-    </html>
-    `;
     const mailOptions = {
       from: senderEmail,
       to: data.email,
-      subject: `HackDavis Hub Password Reset Link`,
+      subject:
+        type === 'invite'
+          ? 'HackDavis Hub Invite Link'
+          : 'HackDavis Hub Password Reset Link',
       replyTo: data.email,
-      html: msg,
+      html: emailMessage(type, invite_link ?? ''),
     };
 
     const output = await new Promise<Response>((resolve, _) => {
