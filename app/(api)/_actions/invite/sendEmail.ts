c3058aa9a25dcfc04827dcc96c65a6140b9cf1ae
brandonw504 <brandonw504@outlook.com>
2025-03-30 22:15:37 -0700

    Fixed invite emailing

--- app/(api)/_actions/invite/sendEmail.ts
+++ app/(api)/_actions/invite/sendEmail.ts
@@ -37,7 +37,7 @@ export default async function sendEmail(
       throw new DuplicateError(`User with email ${data.email} already exists.`);
     }
 
-    data.role = users.body[0].role;
+    if (type === 'reset') data.role = users.body[0].role;
     const invite = await GenerateInvite(data, type);
 
     if (!invite.ok) {
