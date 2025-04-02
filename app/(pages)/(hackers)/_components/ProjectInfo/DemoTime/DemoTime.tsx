07c8b9df46c6fd55c089d33823e830b0b791946f
michelleyeoh <michellew.yeoh@gmail.com>
2025-04-01 20:20:21 -0700

    post demo

--- app/(pages)/(hackers)/_components/ProjectInfo/DemoTime/DemoTime.tsx
+++ app/(pages)/(hackers)/_components/ProjectInfo/DemoTime/DemoTime.tsx
@@ -1,5 +1,6 @@
 import styles from './DemoTime.module.scss';
 import hackathon from '@public/hackers/project-info/hackathon.svg';
+import cow_icon from '@public/hackers/project-info/cow_icon.svg';
 import Image from 'next/image';
 
 export default function DemoTime() {
@@ -89,7 +90,11 @@ export default function DemoTime() {
                 unreachable will be <b>removed from all judge queues.</b>
               </p>
             </div>
-            <Image src={hackathon} alt="hackathon" />
+            <Image
+              src={hackathon}
+              alt="hackathon"
+              className={styles.hackathon_img}
+            />
           </div>
         </div>
       </div>
@@ -102,17 +107,32 @@ export default function DemoTime() {
           className={styles.right_panel}
           style={{ backgroundColor: '#FFC5AB' }}
         >
-          <h3>You will not be visited by a judge in EVERY round.</h3>
-          <p>
-            <b>Why do some teams get more judges? Isn’t it unfair?</b> <br />
-            Some tracks are judged by MLH, partner NPOs, or sponsors selecting
-            their own winners. If your team hasn’t chosen these tracks, you’ll
-            be judged by the standard three judges. Extra judges from these
-            groups won’t affect your chances in other tracks, so having more
-            than three judges doesn’t give an advantage or disadvantage.
-          </p>
-          <p>MLH = Major League Hacking</p>
-          <p>NPO = Non-Profit Organizations</p>
+          <div className={styles.post_demo}>
+            <div className={styles.zero_judges_group}>
+              <Image src={cow_icon} alt="cow icon" />
+              <br />
+              <div className={styles.no_judges}>
+                <p>Zero judges left...</p>
+              </div>
+            </div>
+            <div className={styles.check_out}>
+              <h3>Check out what others are up to!</h3>
+              <p>
+                Once your team has been judged, you're welcome to explore other
+                demos. Please be mindful <b>not to interrupt</b> judges or teams
+                presenting.
+              </p>
+              <p>
+                We kindly ask that you wait 1-2 minutes after the round begins
+                before visiting tables without a judge.
+              </p>
+              <p>
+                If your team experiences any disruptions,{' '}
+                <b>please report them</b> to the Director Table, and we will
+                address them promptly.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
