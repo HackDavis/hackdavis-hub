b0143e3fa5e4c3c033ab19ac4c532b60aa991789
michelleyeoh <michellew.yeoh@gmail.com>
2025-04-02 15:00:58 -0700

    Break section done

--- app/(pages)/(hackers)/_components/ProjectInfo/Break/Break.tsx
+++ app/(pages)/(hackers)/_components/ProjectInfo/Break/Break.tsx
@@ -1,22 +1,34 @@
 import styles from './Break.module.scss';
-import hackathon from '@public/hackers/project-info/hackathon.svg';
+import relaxing_cow from '@public/hackers/project-info/relaxing_cow.svg';
+import radio from '@public/hackers/project-info/radio.svg';
 import Image from 'next/image';
 
 export default function Break() {
   return (
-    <div>
+    <div className={styles.container}>
       <div>
         <h3>Hacker’s Choice Award</h3>
+        <br />
         <p>
           Once demos end, you will about an hour’s time to visit other teams and
-          vote for the Hacker’s Choice Award. You can also look at projects in
-          the gallery on devpost.
+          vote for the <a href="/link">Hacker’s Choice Award</a>. You can also
+          look at projects in the gallery on devpost.
         </p>
+        <br />
         <p>
           Meanwhile, panels of judges will be choosing the winners from the top
           5 projects shortlisted for each track after demos.
         </p>
+        <br />
         <button>Submit Vote</button>
+      </div>
+      <div className={styles.image_cont}>
+        <Image src={radio} alt="radio" className={styles.radio_img} />
+        <Image
+          src={relaxing_cow}
+          alt="relaxing cow"
+          className={styles.cow_img}
+        />
       </div>
     </div>
   );
