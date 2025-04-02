966ea7f41ba6a634447d619a8de643d5b1fc1fd5
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-02 10:37:14 -0700

    Desktop view of Submission Due compete

--- app/(pages)/(hackers)/_components/ProjectInfo/SubmissionDue/SubmissionDue.tsx
+++ app/(pages)/(hackers)/_components/ProjectInfo/SubmissionDue/SubmissionDue.tsx
@@ -5,11 +5,12 @@ export default function SubmissionDue() {
   return (
     <>
       <div className={styles.SubDuecontainer}>
+        <h1 className={styles.heading}> Rubric</h1>
         <div className={styles.boxContainer}>
-          <h1 className={styles.heading}> Rubric</h1>
-          <div className={styles.box}></div>
-          <div className={styles.box}></div>
-          <div className={styles.box}></div>
+          <div className={styles.box}>60% Track-Specific</div>
+          <div className={styles.box}>20% Social Good</div>
+          <div className={styles.box}>10% Creativity</div>
+          <div className={styles.box}>10% Presentation</div>
         </div>
       </div>
     </>
