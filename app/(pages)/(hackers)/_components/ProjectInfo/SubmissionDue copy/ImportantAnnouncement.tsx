a6b7e54af5f1f49fde8a94fc1a80ce69d91c9da5
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-01 20:53:32 -0700

    Imp ANnouncent

--- app/(pages)/(hackers)/_components/ProjectInfo/SubmissionDue copy/ImportantAnnouncement.tsx
+++ app/(pages)/(hackers)/_components/ProjectInfo/SubmissionDue copy/ImportantAnnouncement.tsx
@@ -13,11 +13,31 @@ export default function ImportantAnnouncement() {
             alt="Two animals at a judging table"
           />
         </div>
-        <Image
-          className={styles.step2}
-          src={ImportantAnnouncementImage}
-          alt="Two animals at a judging table"
-        />
+        <div className={styles.child}>
+          <div className={styles.text1}>
+            <p>Team numbers will be available on Devpost</p>
+            <p>
+              Each team member MUST input their team number on <b>HackerHub</b>
+            </p>
+          </div>
+          <div className={styles.text2}>
+            <p>
+              HackerHub will provide you with a <b>table number</b> (NOT the
+              same as your team number)
+            </p>
+            <p>
+              Each team member MUST input their team number on <b>HackerHub</b>
+            </p>
+            <p>
+              Use this table number (NOT your team number) to find your table
+              for demos
+            </p>
+            <p>
+              Contact a HackDavis director if you and your team are not seeing
+              the same table number
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
