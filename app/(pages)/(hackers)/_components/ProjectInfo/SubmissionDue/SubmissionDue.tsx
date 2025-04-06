9a87f37e6717e7b0afe397afa3bda17576587229
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-05 17:20:35 -0700

    SD: Made %s mobile

--- app/(pages)/(hackers)/_components/ProjectInfo/SubmissionDue/SubmissionDue.tsx
+++ app/(pages)/(hackers)/_components/ProjectInfo/SubmissionDue/SubmissionDue.tsx
@@ -4,21 +4,23 @@ import styles from './SubmissionDue.module.scss';
 export default function SubmissionDue() {
   return (
     <>
-      <div className={styles.SubDuecontainer}>
-        <h1 className={styles.heading}> Rubric</h1>
-        <div className={styles.boxContainer}>
-          <div className={styles.box}>
-            <p>60% Track-Specific</p>
-          </div>
-          <div className={styles.box}>
-            <div className={styles.box3}>
-              <p>20% Social Good</p>
+      <div className={styles.pageWrapper}>
+        <div className={styles.SubDuecontainer}>
+          <h1 className={styles.heading}> Rubric</h1>
+          <div className={styles.boxContainer}>
+            <div className={styles.box}>
+              <p>60% Track-Specific</p>
             </div>
-            <div className={styles.box3}>
-              <p>10% Creativity</p>
-            </div>
-            <div className={styles.box3}>
-              <p>10% Presentation</p>
+            <div className={styles.box}>
+              <div className={styles.box3}>
+                <p>20% Social Good</p>
+              </div>
+              <div className={styles.box3}>
+                <p>10% Creativity</p>
+              </div>
+              <div className={styles.box3}>
+                <p>10% Presentation</p>
+              </div>
             </div>
           </div>
         </div>
