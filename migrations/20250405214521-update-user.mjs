f17979e6859f4d532f8371d537962ff5584089e3
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-04-05 21:43:48 -0700

    all other migrations

--- migrations/20250405214521-update-user.mjs
+++ migrations/20250405214521-update-user.mjs
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
-const tracksPath = path.resolve(process.cwd(), 'app/(api)/_data/tracks.json');
-const tracks = JSON.parse(fs.readFileSync(tracksPath, 'utf8'));
+const dataPath = path.resolve(
+  process.cwd(),
+  'app/_data/db_validation_data.json'
+);
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
-const domains = [...new Set(tracks.map((track) => track.type))];
+const domains = [...new Set(data.domains)];
 
 export async function up(db) {
   await db.command({
@@ -69,7 +72,60 @@ export async function up(db) {
 
 export async function down(db) {
   await db.command({
-    collMod: 'yourCollectionName',
-    validator: {}, // Remove the validator
+    collMod: 'users',
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        title: 'Users Object Validation',
+        required: ['name', 'email', 'password', 'role', 'has_checked_in'],
+        properties: {
+          _id: {
+            bsonType: 'objectId',
+            description: '_id must be an ObjectId',
+          },
+          name: {
+            bsonType: 'string',
+            description: 'name must be a string',
+          },
+          email: {
+            bsonType: 'string',
+            pattern: '^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,})$',
+            description: 'email must be a string and have a valid format',
+          },
+          password: {
+            bsonType: 'string',
+            description: 'encrypted password must be a string',
+          },
+          role: {
+            enum: ['hacker', 'judge', 'admin'],
+            description: 'role must be either hacker, judge, or admin',
+          },
+          specialties: {
+            bsonType: 'array',
+            description: 'specialties must be an array of valid string values',
+            maxItems: 3,
+            minItems: 3,
+            items: {
+              enum: ['business', 'tech', 'design'],
+              description: 'specialty must be either tech, business, or design',
+            },
+          },
+          position: {
+            enum: ['developer', 'designer', 'pm', 'other'],
+            description:
+              'position must be either developer, designer, pm, or other',
+          },
+          is_beginner: {
+            bsonType: 'bool',
+            description: 'is_beginner must be a boolean',
+          },
+          has_checked_in: {
+            bsonType: 'bool',
+            description: 'has_checked_in must be a boolean',
+          },
+        },
+        additionalProperties: false,
+      },
+    },
   });
 }
