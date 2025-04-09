36a4847066b8d50a2dadc917fc12f3e5bbe6b58f
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-08 20:53:23 -0700

    imported star

--- app/(pages)/(hackers)/_components/IndexHero/Scroll.tsx
+++ app/(pages)/(hackers)/_components/IndexHero/Scroll.tsx
@@ -1,25 +1,49 @@
 import styles from './Scroll.module.scss';
-// import Image from 'next/image';
+import Image from 'next/image';
+import CowHead from 'public/hackers/hero/CowHead.svg';
+import BunnyHead from 'public/hackers/hero/BunnyHead.svg';
+import FrogHead from 'public/hackers/hero/FrogHead.svg';
+import DuckHead from 'public/hackers/hero/DuckHead.svg';
+import Star from 'public/hackers/hero/Star.svg';
+
+// can delete scroll text
+// make music notes hidden + upon hover notes go up
 
 export default function Scroll() {
   return (
     <div className={styles.container}>
-      <p>star</p>
+      <Image src={Star} alt="Star" className={styles.star} />
       <div className={styles.parent}>
         <div className={styles.child}>
-          <p>icon</p>
+          <Image
+            src={CowHead}
+            alt="Cow Head"
+            // className={styles.vinyl}
+          />
           <p>animals</p>
         </div>
         <div className={styles.child}>
-          <p>icon</p>
+          <Image
+            src={BunnyHead}
+            alt="Bunny Head"
+            // className={styles.vinyl}
+          />
           <p>animals</p>
         </div>
         <div className={styles.child}>
-          <p>icon</p>
+          <Image
+            src={FrogHead}
+            alt="Frog Head"
+            // className={styles.vinyl}
+          />
           <p>animals</p>
         </div>
         <div className={styles.child}>
-          <p>icon</p>
+          <Image
+            src={DuckHead}
+            alt="Duck Head"
+            // className={styles.vinyl}
+          />
           <p>animals</p>
         </div>
       </div>
