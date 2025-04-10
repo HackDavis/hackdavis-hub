60040adbc05aa9a44854faf632a5cfbe85bc3e22
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-04-09 17:55:01 -0700

    restructured algo files in _utils, moved judgeToPanel algo to utils and optimized it

---
+++ app/(api)/_utils/matching/judgeToPanelAlgorithm.ts
@@ -0,0 +1,24 @@
+import Panel from '@typeDefs/panel';
+import User from '@typeDefs/user';
+
+export default async function judgeToPanelAlgorithm(
+  panels: Panel[],
+  judges: User[],
+  maxPanelSize: number = 5
+) {
+  for (const panel of panels) {
+    if (judges.length < 2) return null;
+
+    judges = judges.sort(
+      (a, b) =>
+        (a.specialties?.indexOf(panel.domain) ?? 0) -
+        (b.specialties?.indexOf(panel.domain) ?? 0)
+    );
+
+    panel.user_ids = judges
+      .splice(0, maxPanelSize)
+      .map((judge) => judge._id ?? '');
+  }
+
+  return panels;
+}
