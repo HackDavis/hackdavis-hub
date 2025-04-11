284b3b1b9670f86dd04b75b01ce14c78fe64262f
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-04-10 17:33:46 -0700

    fixed starterKitSlide and added scrollToTop

--- app/(pages)/(hackers)/_components/StarterKit/StarterKitSlide.tsx
+++ app/(pages)/(hackers)/_components/StarterKit/StarterKitSlide.tsx
@@ -4,20 +4,30 @@ interface StarterKitSlideProps {
   subtitle: string;
   title: string;
   children: React.ReactNode;
+  route?: string;
 }
 
 export default function StarterKitSlide({
   title,
   subtitle,
   children,
+  route = 'starter-kit',
 }: StarterKitSlideProps) {
   return (
     <Card className="h-fit w-full p-0 bg-transparent shadow-none border-none text-text-dark">
       <CardContent className="p-0">
-        <p className="text-[0.5rem] xs:text-xs md:text-sm font-jakarta tracking-[0.02em]">
+        <p
+          className={`text-[0.5rem] xs:text-xs font-jakarta tracking-[0.02em] ${
+            route === 'starter-kit' ? 'md:text-sm' : 'md:text-lg'
+          }`}
+        >
           {subtitle}
         </p>
-        <h2 className="text-lg xs:xl md:text-2xl font-bold font-metropolis tracking-[0.02em] mb-4">
+        <h2
+          className={`text-lg xs:xl font-bold font-metropolis tracking-[0.02em]  ${
+            route === 'starter-kit' ? 'md:text-2xl mb-4' : 'md:text-4xl'
+          }`}
+        >
           {title}
         </h2>
         {children}
