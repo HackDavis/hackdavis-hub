30a2cf554a7655c717675f4955fcf4e8f1d2af01
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-13 18:40:30 -0700

    Flex-end

--- app/(pages)/(hackers)/_components/Contact/Contact.tsx
+++ app/(pages)/(hackers)/_components/Contact/Contact.tsx
@@ -22,23 +22,25 @@ export default function Contact() {
           </div>
         </div>
         <div className={styles.blueBox}>
-          <p>
-            MENTORS can help you with supporting developers and designers with
-            any technical questions regarding their projects! Contact one now to
-            ask about blah blah blah
-          </p>
-          <button className={styles.button}>CONTACT A Mentor</button>
+          <div className={styles.textContent}>
+            <button className={styles.button}>CONTACT A Mentor</button>
+            <p>
+              MENTORS can help you with supporting developers and designers...
+            </p>
+          </div>
         </div>
         <div className={styles.whiteBox}></div>
       </div>
       <div className={styles.container}>
         <div className={styles.whiteBox}></div>
         <div className={styles.blueBox}>
-          <p>
-            DIRECTORS can help you with any questions regarding hackathon
-            events, planning, whatever whatever whatever!
-          </p>
-          <button className={styles.button}>CONTACT A Director</button>
+          <div className={styles.textContent}>
+            <p>
+              DIRECTORS can help you with any questions regarding hackathon
+              events, planning, whatever whatever whatever!
+            </p>
+            <button className={styles.button}>CONTACT A Director</button>
+          </div>
         </div>
         <div className={styles.whiteBox}>
           <div className={styles.animalWrapper}>
