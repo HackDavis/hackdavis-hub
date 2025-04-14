d25d17e35f7f725eeac929ff96c9ce9a3d74e9e8
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-13 19:31:45 -0700

    Added mobile start

--- app/(pages)/(hackers)/_components/Contact/Contact.tsx
+++ app/(pages)/(hackers)/_components/Contact/Contact.tsx
@@ -8,65 +8,116 @@ import Duck from 'public/hackers/hero/AirpodDuck.svg';
 
 export default function Contact() {
   return (
-    <div className={styles.topSection}>
-      <div className={styles.container}>
-        <div className={styles.blueBox}></div>
-        <div className={styles.whiteBox}>
-          <div className={styles.animalWrapper}>
-            <Image src={Cow} alt="Cow holding ipod" className={styles.cow} />
+    <>
+      <div className={styles.topSection}>
+        <div className={styles.container}>
+          <div className={styles.blueBox}></div>
+          <div className={styles.whiteBox}>
+            <div className={styles.animalWrapper}>
+              <Image src={Cow} alt="Cow holding ipod" className={styles.cow} />
+              <Image
+                src={CowPhone}
+                alt="Cow holding ipod"
+                className={styles.cowphone}
+              />
+            </div>
+          </div>
+          <div
+            className={`${styles.blueBox} ${styles.textBox} ${styles.alignBottom}`}
+          >
+            <div className={styles.textContent}>
+              <button className={styles.button}>CONTACT A Mentor</button>
+              <p>
+                MENTORS are here to support developers and designers with any
+                technical challenges you might face during your project. Whether
+                you're debugging, designing, or stuck on a problem, mentors are
+                here to help.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.whiteBox}></div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.whiteBox}></div>
+          <div className={styles.blueBox}>
+            <div className={styles.textContent}>
+              <p>
+                DIRECTORS can answer any questions you have about the hackathon
+                itself — from logistics and scheduling to rules and submissions.
+                If you're unsure where to go or what to do, they're your go-to
+                guide.
+              </p>
+              <button className={styles.button}>CONTACT A Director</button>
+            </div>
+          </div>
+          <div className={styles.whiteBox}>
+            <div className={styles.animalWrapper}>
+              <Image
+                src={Duck}
+                alt="Duck holding ipod"
+                className={styles.duck}
+              />
+              <Image
+                src={DuckPhone}
+                alt="Duck holding ipod"
+                className={styles.duckphone}
+              />
+            </div>
+          </div>
+          <div className={styles.blueBox}></div>
+        </div>
+        <Image
+          src={Squiggle}
+          alt="Squiggle blue line divider"
+          className={styles.squiggle}
+        />
+      </div>
+
+      {/* mobile section starts here */}
+      {/* <!-- Mentor Card --> */}
+      <div className={styles.mobile}>
+        <div className={styles.cardMentor}>
+          <div className={styles.mobileanimalWrapper}>
+            <Image
+              src={Duck}
+              alt="Duck holding ipod"
+              className={styles.mobileduck}
+            />
             <Image
               src={CowPhone}
-              alt="Cow holding ipod"
-              className={styles.cowphone}
+              alt="Duck holding ipod"
+              className={styles.mobilecowphone}
             />
           </div>
-        </div>
-        <div
-          className={`${styles.blueBox} ${styles.textBox} ${styles.alignBottom}`}
-        >
-          <div className={styles.textContent}>
-            <button className={styles.button}>CONTACT A Mentor</button>
-            <p>
-              MENTORS are here to support developers and designers with any
-              technical challenges you might face during your project. Whether
-              you're debugging, designing, or stuck on a problem, mentors are
-              here to help.
-            </p>
-          </div>
+          <p>
+            <strong>MENTORS</strong> can support you with feedback in
+            development and designing. Feel free to call one over!
+          </p>
+          <button>CONTACT A MENTOR</button>
         </div>
 
-        <div className={styles.whiteBox}></div>
-      </div>
-      <div className={styles.container}>
-        <div className={styles.whiteBox}></div>
-        <div className={styles.blueBox}>
-          <div className={styles.textContent}>
-            <p>
-              DIRECTORS can answer any questions you have about the hackathon
-              itself — from logistics and scheduling to rules and submissions.
-              If you're unsure where to go or what to do, they're your go-to
-              guide.
-            </p>
-            <button className={styles.button}>CONTACT A Director</button>
-          </div>
-        </div>
-        <div className={styles.whiteBox}>
-          <div className={styles.animalWrapper}>
-            <Image src={Duck} alt="Duck holding ipod" className={styles.duck} />
+        {/* <!-- Director Card --> */}
+        <div className={styles.cardDirector}>
+          <div className={styles.mobileanimalWrapper}>
+            <Image
+              src={Duck}
+              alt="Duck holding ipod"
+              className={styles.mobileduck}
+            />
             <Image
               src={DuckPhone}
               alt="Duck holding ipod"
-              className={styles.duckphone}
+              className={styles.mobileduckphone}
             />
-          </div>
+          </div>{' '}
+          <p>
+            <strong>DIRECTORS</strong> can help you with any questions regarding
+            hackathon events, schedules, and overall logistics!
+          </p>
+          <button>CONTACT A DIRECTOR</button>
         </div>
-        <div className={styles.blueBox}></div>
       </div>
-      <Image
-        src={Squiggle}
-        alt="Squiggle blue line divider"
-        className={styles.squiggle}
-      />
-    </div>
+    </>
   );
 }
