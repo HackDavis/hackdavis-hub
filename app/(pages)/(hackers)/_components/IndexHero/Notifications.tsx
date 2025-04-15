3feada961b5cdbca3b15e63937c6f161ec6689d3
Haylie Tan <haylie.tan.code@gmail.com>
2025-04-14 20:50:35 -0700

    Trying to fix build errors

--- app/(pages)/(hackers)/_components/IndexHero/Notifications.tsx
+++ app/(pages)/(hackers)/_components/IndexHero/Notifications.tsx
@@ -5,72 +5,69 @@ import Image from 'next/image';
 import NotificationIndividual from './NotificationIndividual';
 import notif_new from '@public/hackers/hero/notif_new.svg';
 
-export default function Notifications(){
-    const [isNew, setIsNew] = useState(true);
-    const [isOpen, setIsOpen] = useState(false);
+export default function Notifications() {
+  const [isNew, setIsNew] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-        setIsNew(false);
-    };
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    setIsNew(false);
+  };
 
-    const [notifications, setNotifications] = useState([
-        {
-          id: 1,
-          name: 'MLH Track',
-          description: 'Since you submitted for an MLH track, you will also be visited by a representative.',
-        },
-        {
-          id: 2,
-          name: 'NPO Track',
-          description: 'Since you submitted for an NPO track, you will also be visited by a representative.',
-        },
-        {
-          id: 3,
-          name: 'Reminder',
-          description: 'You and your team need to be present at your assigned table during judging!',
-        },
-      ]);
-    
-      const dismissNotification = (id) => {
-        setNotifications((prev) => prev.filter((n) => n.id !== id));
-      };
+  const [notifications, setNotifications] = useState([
+    {
+      id: 1,
+      name: 'MLH Track',
+      description:
+        'Since you submitted for an MLH track, you will also be visited by a representative.',
+    },
+    {
+      id: 2,
+      name: 'NPO Track',
+      description:
+        'Since you submitted for an NPO track, you will also be visited by a representative.',
+    },
+    {
+      id: 3,
+      name: 'Reminder',
+      description:
+        'You and your team need to be present at your assigned table during judging!',
+    },
+  ]);
 
-      useEffect(() => {
-        if (notifications.length === 0) {
-          setIsOpen(false);
-        }
-      }, [notifications]);
+  const dismissNotification = (id) => {
+    setNotifications((prev) => prev.filter((n) => n.id !== id));
+  };
 
-    return(
+  useEffect(() => {
+    if (notifications.length === 0) {
+      setIsOpen(false);
+    }
+  }, [notifications]);
+
+  return (
     <div>
-        <button onClick={handleClick}>
-            {!isOpen && (
-                <Image 
-                    src={isNew ? notif_new : notif} 
-                    alt="notification" 
-                />
-            )}
-        </button>
-        {isOpen && (
-            <div className={styles.container_position}>
-                {notifications.length === 0 ? (
-                    <Image 
-                    src={notif} 
-                    alt="notification" 
-                />
-                ) : (
-                notifications.map((notif) => (
-                    <NotificationIndividual
-                    key={notif.id}
-                    name={notif.name}
-                    description={notif.description}
-                    onDismiss={() => dismissNotification(notif.id)}
-                    />
-                ))
-                )}
-            </div>
+      <button onClick={handleClick}>
+        {!isOpen && (
+          <Image src={isNew ? notif_new : notif} alt="notification" />
         )}
-      </div>
-    );
+      </button>
+      {isOpen && (
+        <div className={styles.container_position}>
+          {notifications.length === 0 ? (
+            <Image src={notif} alt="notification" />
+          ) : (
+            notifications.map((notif) => (
+              <NotificationIndividual
+                key={notif.id}
+                name={notif.name}
+                description={notif.description}
+                onDismiss={() => dismissNotification(notif.id)}
+              />
+            ))
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
--- app/(pages)/judges/(app)/_components/JudgingHub/JudgingHub.tsx
+++ app/(pages)/judges/(app)/_components/JudgingHub/JudgingHub.tsx
@@ -5,8 +5,8 @@ import TableLocations from './TableLocations';
 import ViewProjects from './ViewProjects';
 import styles from './JudgingHub.module.scss';
 import Waiting from './Waiting';
-import ViewMap from './ViewMap';
-import Dismiss from './Dismiss';
+// import ViewMap from './ViewMap';
+// import Dismiss from './Dismiss';
 
 export default function JudgingHub() {
   return (
