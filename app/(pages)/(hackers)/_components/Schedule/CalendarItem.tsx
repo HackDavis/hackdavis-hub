2dd3660d5bd89d9a7d19b9b9b3fddc205e9d0d6b
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-04-16 21:28:53 -0700

    fixes (this code is not modular holy moly)

--- app/(pages)/(hackers)/_components/Schedule/CalendarItem.tsx
+++ app/(pages)/(hackers)/_components/Schedule/CalendarItem.tsx
@@ -125,36 +125,38 @@ export function CalendarItem({
             </div>
           )}
         </div>
-        <div
-          className={`flex flex-col sm:flex-row justify-between items-center gap-4 sm:gap-6 w-full ${
-            //bottom
-            displayType !== 'ACTIVITIES' ? '' : 'sm:w-auto'
-          }`}
-        >
+        {displayType !== 'GENERAL' && displayType !== 'MEALS' && (
           <div
-            className={`flex gap-2 items-center w-full sm:w-auto ${
-              displayType === 'WORKSHOPS' &&
-              attendeeCount !== undefined &&
-              attendeeCount > 0
-                ? 'visible'
-                : 'invisible'
+            className={`flex flex-col sm:flex-row justify-between items-center gap-4 sm:gap-6 w-full ${
+              //bottom
+              displayType !== 'ACTIVITIES' ? '' : 'sm:w-auto'
             }`}
           >
-            <div className="relative w-16 h-12">
-              <Image
-                src="/index/schedule/attendee.svg"
-                alt="location icon"
-                fill
-              />
-            </div>
-            <span className="text-black font-plus-jakarta-sans text-xs xs:text-sm md:text-base lg:text-lg font-normal leading-[145%] tracking-[0.36px] text-balance">
-              {`${attendeeCount ?? ''} Hacker${
-                attendeeCount && attendeeCount < 2 ? ' is' : 's are'
-              } attending this event`}
-            </span>
-          </div>
+            {displayType === 'WORKSHOPS' && (
+              <div
+                className={`flex gap-2 items-center w-full sm:w-auto ${
+                  attendeeCount && attendeeCount > 0
+                    ? 'visible'
+                    : 'hidden sm:invisible sm:flex'
+                }
+              
+                  `}
+              >
+                <div className="relative w-16 h-12">
+                  <Image
+                    src="/index/schedule/attendee.svg"
+                    alt="location icon"
+                    fill
+                  />
+                </div>
+                <span className="text-black font-plus-jakarta-sans text-xs xs:text-sm md:text-base lg:text-lg font-normal leading-[145%] tracking-[0.36px] text-balance">
+                  {`${attendeeCount ?? ''} Hacker${
+                    attendeeCount && attendeeCount < 2 ? ' is' : 's are'
+                  } attending this event`}
+                </span>
+              </div>
+            )}
 
-          {displayType !== 'GENERAL' && displayType !== 'MEALS' && (
             <div className="flex flex-col gap-2 items-end w-full sm:w-auto">
               <Button
                 onClick={
@@ -173,8 +175,8 @@ export function CalendarItem({
                 </p>
               </Button>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
