f8512e68549f7442262ac4e713434706f695457e
reehals <sunny.reehal@gmail.com>
2025-04-18 21:25:42 -0700

    match page

---
+++ app/(pages)/admin/match/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import JudgeTeamGrouping from '../_components/JudgeTeamGrouping/JudgeTeamGrouping';
+
+export default function ScoreAndShortlist() {
+  return <JudgeTeamGrouping />;
+}
