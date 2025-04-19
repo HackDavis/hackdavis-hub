516108b4fe933b46a6672acaa6c502dec77420eb
brandonw504 <brandonw504@outlook.com>
2025-04-18 23:28:18 -0700

    Removed ClientSessionProvider

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-// import ClientSessionProvider from './_components/ClientSessionProvider/ClientSessionProvider';
 import { SessionProvider } from 'next-auth/react';
 import '@globals/globals.scss';
 import metadataJSON from '@globals/metadata.json';
@@ -15,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${fonts} antialiased`}>
-        {/* <ClientSessionProvider>{children}</ClientSessionProvider> */}
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
