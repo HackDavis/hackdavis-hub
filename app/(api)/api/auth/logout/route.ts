3373844323c3e5f505051f9253213b6eaf783f18
phlangiee <ava2007mara@gmail.com>
2025-09-21 22:09:58 -0700

    Added a param to post function

--- app/(api)/api/auth/logout/route.ts
+++ app/(api)/api/auth/logout/route.ts
@@ -1,11 +1,11 @@
 'use server';
 
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 import Logout from '@datalib/auth/logout';
 import authenticated from '@utils/authentication/authenticated';
 
-async function post() {
+async function post(request: NextRequest) {
   const res = await Logout();
   return NextResponse.json({ ...res }, { status: res.ok ? 200 : 401 });
 }
