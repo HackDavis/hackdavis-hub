b4fcad6de975a14b11f73659637c8597fccff440
phlangiee <ava2007mara@gmail.com>
2025-09-21 23:24:22 -0700

    More formmatting

--- app/(pages)/admin/teams/page.tsx
+++ app/(pages)/admin/teams/page.tsx
@@ -75,30 +75,28 @@ export default function Teams() {
       <div>
         <div className={styles.reported_teams_container}>
           <button onClick={toggleReportedTeamsDisplay}>
-            {reportedTeamsDisplay ? "v" : ">"}
+            {reportedTeamsDisplay ? 'v' : '>'}
           </button>
           <h2 className={styles.action_header}> Reported Teams</h2>
         </div>
         <div className={styles.reports_container}>
           {reportedTeamsDisplay &&
             reportedTeams.map((team) => (
-              <a className={styles.report_container} 
-              key={team._id}
-              >
+              <div className={styles.report_container} key={team._id}>
                 <a href={`#${team._id}`}>
                   Team #{team.teamNumber}: {team.name} @ Table {team.tableNumber}
                 </a>
-              </a>
+              </div>
             ))}
         </div>
       </div>
       <div className={styles.data_portion}>
         <div className={styles.teams_list}>
           {teamData.map((team: TeamWithJudges) => (
-            <div 
-            id={team._id}
-            className={styles.team_card_wrapper}
-            key={team._id}
+            <div
+              id={team._id}
+              className={styles.team_card_wrapper}
+              key={team._id}
             >
               <TeamCard team={team} onEditClick={() => setData(team)} />
             </div>
