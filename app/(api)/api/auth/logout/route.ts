bc246ab0c60f66629a94c37a16d5165695fe70a9
phlangiee <ava2007mara@gmail.com>
2025-09-21 23:58:32 -0700

    Maybe this complies??

--- app/(api)/api/auth/logout/route.ts
+++ app/(api)/api/auth/logout/route.ts
@@ -5,7 +5,7 @@ import { NextResponse, NextRequest } from 'next/server';
 import Logout from '@datalib/auth/logout';
 import authenticated from '@utils/authentication/authenticated';
 
-async function post(_: NextRequest) {
+async function post(__: NextRequest) {
   const res = await Logout();
   return NextResponse.json({ ...res }, { status: res.ok ? 200 : 401 });
 }
