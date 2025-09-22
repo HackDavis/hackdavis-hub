a10fc8a9b804f9f68a5d7fe9ca62516d74a6850e
phlangiee <ava2007mara@gmail.com>
2025-09-21 22:26:48 -0700

    Meshed the post function with the export line (please please please work)

--- app/(api)/api/auth/logout/route.ts
+++ app/(api)/api/auth/logout/route.ts
@@ -5,9 +5,7 @@ import { NextResponse, NextRequest } from 'next/server';
 import Logout from '@datalib/auth/logout';
 import authenticated from '@utils/authentication/authenticated';
 
-async function post(request: NextRequest) {
+export const POST = authenticated(async (request: NextRequest) => {
   const res = await Logout();
   return NextResponse.json({ ...res }, { status: res.ok ? 200 : 401 });
-}
-
-export const POST = authenticated(post);
+});
