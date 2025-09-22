fa5db7e6a6e613ed8caf8b6faa6d9efa17edf989
phlangiee <ava2007mara@gmail.com>
2025-09-21 18:49:20 -0700

    test

--- app/(api)/api/auth/logout/route.ts
+++ app/(api)/api/auth/logout/route.ts
@@ -1,11 +1,25 @@
 'use server';
+/*
+import { NextResponse } from 'next/server';
+
+import Logout from '@datalib/auth/logout';
+import authenticated from '@utils/authentication/authenticated';
+
+async function post() {
+  const res = await Logout();
+  return NextResponse.json({ ...res }, { status: res.ok ? 200 : 401 });
+}
+
+const POST = authenticated(post);
+
+export { POST };*/
 
 import { NextResponse, NextRequest } from 'next/server';
 
 import Logout from '@datalib/auth/logout';
 import authenticated from '@utils/authentication/authenticated';
 
-async function post(request: NextRequest, context: { params: any }) {
+async function post(request: NextRequest) {
   const res = await Logout();
   return NextResponse.json({ ...res }, { status: res.ok ? 200 : 401 });
 }
