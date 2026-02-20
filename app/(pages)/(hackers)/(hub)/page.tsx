91f9b0f65b643e3ba789601e31944b30eadf008e
jackzheng-cs <134164876+jackzheng-cs@users.noreply.github.com>
2026-02-19 22:23:57 -0800

    Feat/mentor and director help (#382)
    * chore: add new contact file
    
    * feat: add mobile and desktop mentor-and-director-help card component
    
    * fixed padding on mobile
    
    * undo auth
    
    ---------
    
    Co-authored-by: clarayin129 <clarayin129@gmail.com>
    Co-authored-by: michelleyeoh <74385095+michelleyeoh@users.noreply.github.com>
    Co-authored-by: michelleyeoh <michellew.yeoh@gmail.com>

--- app/(pages)/(hackers)/(hub)/page.tsx
+++ app/(pages)/(hackers)/(hub)/page.tsx
@@ -1,15 +1,15 @@
-import PrizeTracks from '@pages/(hackers)/_components/PrizeTracks/PrizeTracks';
+// import PrizeTracks from '@pages/(hackers)/_components/PrizeTracks/PrizeTracks';
 import BeginnersSection from '@pages/(hackers)/_components/BeginnersSection/BeginnersSection';
 import Discord from '@pages/(hackers)/_components/StayUpToDate/Discord';
 import Footer from '@components/Footer/Footer';
 import Hero from '../_components/Hero/Hero';
-import Contact from '@pages/(hackers)/_components/Contact/Contact';
 // import IndexHero from '@pages/(hackers)/_components/IndexHero/IndexHero';
 // import IndexHeroContentHacking from '@pages/(hackers)/_components/DOE/Hacking/IndexHeroContentHacking';
 // import IndexHeroContentJudging from '@pages/(hackers)/_components/DOE/Judging/IndexHeroContentJudging';
 import ClientTimeProtectedDisplay from '@pages/_components/TimeProtectedDisplay/ClientTimeProtectedDisplay';
 import TableNumberCheckin from '@pages/(hackers)/_components/TableNumberCheckin/TableNumberCheckin';
 import TableNumberContextProvider from '@pages/_contexts/TableNumberContext';
+import MDHelp from '@pages/(hackers)/_components/MentorAndDirectorHelp/MDHelp';
 
 export default function Page() {
   return (
@@ -29,9 +29,9 @@ export default function Page() {
         </ClientTimeProtectedDisplay>
       </TableNumberContextProvider>
       <BeginnersSection />
-      <Contact />
+      <MDHelp />
       <Discord />
-      <PrizeTracks />
+      {/* <PrizeTracks /> */}
       <Footer />
     </main>
   );
