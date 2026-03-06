67998a9f04581b90ad69650685513c919868d8bd
Haylie Tan <haytan@ucdavis.edu>
2026-03-05 16:41:33 -0800

    linting

--- app/(pages)/(hackers)/(hub)/page.tsx
+++ app/(pages)/(hackers)/(hub)/page.tsx
@@ -2,10 +2,10 @@ import PrizeTracks from '@pages/(hackers)/_components/PrizeTracks/PrizeTracks';
 import BeginnersSection from '@pages/(hackers)/_components/HomeHacking/BeginnersSection';
 import Discord from '@pages/(hackers)/_components/StayUpToDate/Discord';
 import Footer from '@components/Footer/Footer';
-// import HeroMVP from '../_components/HomeHacking/HeroMVP';
-// import ClientTimeProtectedDisplay from '@pages/_components/TimeProtectedDisplay/ClientTimeProtectedDisplay';
-// import TableNumberCheckin from '@pages/(hackers)/_components/TableNumberCheckin/TableNumberCheckin';
-// import TableNumberContextProvider from '@pages/_contexts/TableNumberContext';
+import HeroMVP from '../_components/HomeHacking/HeroMVP';
+import ClientTimeProtectedDisplay from '@pages/_components/TimeProtectedDisplay/ClientTimeProtectedDisplay';
+import TableNumberCheckin from '@pages/(hackers)/_components/TableNumberCheckin/TableNumberCheckin';
+import TableNumberContextProvider from '@pages/_contexts/TableNumberContext';
 import MDHelp from '@pages/(hackers)/_components/HomeHacking/MDHelp';
 import ScheduleSneakPeek from '@pages/(hackers)/_components/HomeHacking/ScheduleSneakPeek';
 import HeroJudging from '../_components/HomeJudging/HeroJudging';
@@ -16,24 +16,24 @@ import IndexHeroContentHacking from '../_components/HomeHacking/IndexHeroContent
 export default function Page() {
   return (
     <main id="home">
-      {/* <TableNumberContextProvider> */}
-      {/* <ClientTimeProtectedDisplay featureId="hero-hacking"> */}
-      {/* <HeroMVP /> */}
-      <IndexHeroContentHacking />
-      <ScheduleSneakPeek />
-      <BeginnersSection />
-      <MDHelp />
-      {/* </ClientTimeProtectedDisplay> */}
-      {/* temporarilty set featureId below to "hero-hacking" to test */}
-      {/* <ClientTimeProtectedDisplay featureId="hero-hacking"> */}
-      <HeroWaiting />
-      <HeroJudging />
-      <HackerChoiceAward />
-      {/* </ClientTimeProtectedDisplay> */}
-      {/* <ClientTimeProtectedDisplay featureId="table-number-checkin"> */}
-      {/* <TableNumberCheckin /> */}
-      {/* </ClientTimeProtectedDisplay> */}
-      {/* </TableNumberContextProvider> */}
+      <TableNumberContextProvider>
+        <ClientTimeProtectedDisplay featureId="hero-hacking">
+          <HeroMVP />
+          <IndexHeroContentHacking />
+          <ScheduleSneakPeek />
+          <BeginnersSection />
+          <MDHelp />
+        </ClientTimeProtectedDisplay>
+        {/* temporarilty set featureId below to "hero-hacking" to test */}
+        <ClientTimeProtectedDisplay featureId="hero-hacking">
+          <HeroWaiting />
+          <HeroJudging />
+          <HackerChoiceAward />
+        </ClientTimeProtectedDisplay>
+        <ClientTimeProtectedDisplay featureId="table-number-checkin">
+          <TableNumberCheckin />
+        </ClientTimeProtectedDisplay>
+      </TableNumberContextProvider>
       <Discord />
       <PrizeTracks />
       <Footer />
