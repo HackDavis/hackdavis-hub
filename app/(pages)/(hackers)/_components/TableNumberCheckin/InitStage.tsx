f962e7dc23f4dbf8f3092a9221772fd6a4a9629f
Jack Zheng <jackzheng274@gmail.com>
2026-03-12 15:20:22 -0700

    feat: add initial stage desktop

--- app/(pages)/(hackers)/_components/TableNumberCheckin/InitStage.tsx
+++ app/(pages)/(hackers)/_components/TableNumberCheckin/InitStage.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 
 interface InitStageProps {
   onNext: () => void;
@@ -8,29 +8,35 @@ interface InitStageProps {
 
 export default function InitStage({ onNext }: InitStageProps) {
   return (
-    <div className="flex flex-col mb-[5%] p-[20px] h-[556px] gap-4 rounded-[20px] bg-[#FAFAFF]">
-      {/* Image - top half */}
-      <div className="relative w-full h-1/2">
+    <div
+      className="flex flex-col mb-[5%] p-[20px] h-[556px] gap-4 rounded-[20px] bg-[#FAFAFF]
+                    md:flex-row md:h-[569] md:items-center md:p-[60px]"
+    >
+      {/* Image - top half on mobile, right side on desktop */}
+      <div className="relative w-full h-1/2 md:order-2 md:h-full md:w-1/2 md:rounded-[16px] md:overflow-hidden">
         <Image
-          src={'./hackers/table-number-checkin/end-of-hackathon.svg'}
+          src={"./hackers/table-number-checkin/end-of-hackathon.svg"}
           alt="mascots hanging out"
           fill
           className="object-cover"
         />
       </div>
 
-      {/* Content - bottom */}
-      <div className="flex flex-col justify-between h-1/2">
+      {/* Content - bottom on mobile, left side on desktop */}
+      <div className="flex flex-col justify-between h-1/2 md:h-full md:w-1/2">
         <div className="flex flex-col gap-1 text-black">
-          <h3 className="text-lg font-semibold">The Hackathon has ended!</h3>
-          <p className="text-lg font-semibold text-[#878796]">
+          <h3 className="text-md font-semibold md:text-[32px]">
+            The Hackathon has ended!
+          </h3>
+          <p className="text-md font-semibold text-[#878796] md:text-[32px] leading-normal ">
             Thank you for all your hard work. Next, please follow the directions
             to find your assigned table number.
           </p>
         </div>
+
         <div className="flex flex-row w-full">
           <button
-            className="text-[#1A3819] font-semibold text-base flex justify-center items-center px-8 py-3 rounded-[40px] bg-[#CCFFFE] cursor-pointer shrink-0"
+            className="text-[#1A3819] font-semibold text-base flex justify-center items-center px-8 py-3 rounded-[40px] bg-[#CCFFFE] cursor-pointer shrink-0 md:px-[44px] md:py-5"
             onClick={() => {
               onNext();
             }}
@@ -38,7 +44,7 @@ export default function InitStage({ onNext }: InitStageProps) {
             Ready to find my table
             <div className="relative w-6 h-6 ml-2">
               <Image
-                src={'./hackers/table-number-checkin/arrow-right.svg'}
+                src={"./hackers/table-number-checkin/arrow-right.svg"}
                 alt="Right Arrow"
                 fill
                 className="object-cover"
