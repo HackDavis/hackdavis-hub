1160335011eb9c3c20ae175b47e174a5303bd690
Jack Zheng <jackzheng274@gmail.com>
2026-03-11 19:55:33 -0700

    feat: add first mobile card

--- app/(pages)/(hackers)/_components/TableNumberCheckin/TableNumberCheckin.tsx
+++ app/(pages)/(hackers)/_components/TableNumberCheckin/TableNumberCheckin.tsx
@@ -1,18 +1,15 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
-import { useTableNumber } from '@pages/_hooks/useTableNumber';
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { useTableNumber } from "@pages/_hooks/useTableNumber";
 
-import stars from '@public/hackers/stars.svg';
-import mascots from '@public/hackers/mascots-hanging-out.svg';
-import sleepyFrog from '@public/hackers/sleeping-frog.svg';
-import modalArrow from '@public/hackers/modal-arrow.svg';
-import diagArrow from '@public/hackers/diag-arrow.svg';
+import sleepyFrog from "@public/hackers/sleeping-frog.svg";
+import modalArrow from "@public/hackers/modal-arrow.svg";
+import diagArrow from "@public/hackers/diag-arrow.svg";
 
-import styles from './TableNumberCheckin.module.scss';
-import useTableNumberContext from '@pages/_hooks/useTableNumberContext';
+import useTableNumberContext from "@pages/_hooks/useTableNumberContext";
 
 export default function TableNumberCheckin() {
   const {
@@ -21,7 +18,7 @@ export default function TableNumberCheckin() {
     setValue,
   } = useTableNumberContext();
 
-  const [teamNumber, setTeamNumber] = useState<string>('');
+  const [teamNumber, setTeamNumber] = useState("");
   const { loading, tableNumber, fetchTableNumber, setTableNumber, error } =
     useTableNumber();
 
@@ -29,11 +26,11 @@ export default function TableNumberCheckin() {
     return null;
   }
 
-  let stage: 'init' | 'loading' | 'confirm' = 'init';
+  let stage = "init";
   if (loading) {
-    stage = 'loading';
+    stage = "loading";
   } else {
-    stage = tableNumber ? 'confirm' : 'init';
+    stage = tableNumber ? "confirm" : "init";
   }
 
   const handleTeamNumberSubmit = () => {
@@ -41,50 +38,70 @@ export default function TableNumberCheckin() {
   };
 
   const hasTeamNumber = Boolean(teamNumber);
+
   const inputStage = (
-    <div className={styles.init_content_container}>
-      <div className={styles.mascot_container}>
-        <Image src={stars} alt={'floating stars'} />
+    <div className="flex flex-col p-[20px] mb-[5%] h-[556px] gap-4 rounded-[20px] bg-[#FAFAFF]">
+      {/* Image - top half */}
+      <div className="relative w-full h-1/2">
         <Image
-          src={mascots}
-          alt={'mascots hanging out'}
-          className={styles.mascots}
+          src={"./hackers/end_of_hackathon.svg"}
+          alt="mascots hanging out"
+          fill
+          className="object-cover"
         />
       </div>
-      <div className={styles.text_container}>
-        <h3>THE HACKATHON HAS ENDED</h3>
-        <p>
-          Thank you for all your hard work during the past 24 hours, HackDavis
-          recognizes your passion and talent. Please enter in the team number
-          you received from Devpost.
-        </p>
-      </div>
-      <div className={styles.input_container}>
-        <input
+
+      {/* Content - bottom */}
+      <div className="flex flex-col justify-between h-1/2">
+        <div className="flex flex-col gap-1 text-black">
+          <h3 className="text-lg font-semibold">The Hackathon has ended!</h3>
+          <p className="text-lg font-semibold text-[#878796]">
+            Thank you for all your hard work. Next, please follow the directions
+            for find your assigned table number.
+          </p>
+        </div>
+        <div className="flex flex-row gap-11 w-full">
+          {/* <input
           type="text"
-          className={`${styles.team_input} ${error ? styles.error : null}`}
+          className={`rounded-2xl bg-white/70 border-transparent px-4 py-4 grow min-w-0 text-[#005271] text-lg border-none focus:outline-none focus:ring-0 placeholder:text-[#005271] tablet-s:text-sm tablet-s:py-2 desktop-s:text-base ${
+            error ? "border border-red-500" : ""
+          }`}
           placeholder="#####"
           value={teamNumber}
-          onChange={(event) => setTeamNumber(event.target.value)}
-        />
-        <button
-          className={styles.see_teammates_button}
-          disabled={!hasTeamNumber}
-          style={{ opacity: hasTeamNumber ? 1.0 : 0.3 }}
-          onClick={handleTeamNumberSubmit}
-        >
-          See my teammates! <Image src={modalArrow} alt="" />
-        </button>
+          onChange={(e) => setTeamNumber(e.target.value)}
+        /> */}
+          <button
+            className="text-[#1A3819] font-semibold text-base flex justify-center items-center px-8 py-3 rounded-[40px] bg-[#CCFFFE] cursor-pointer shrink-0 "
+            disabled={!hasTeamNumber}
+            onClick={handleTeamNumberSubmit}
+          >
+            Ready to find my table
+            <div className="relative w-6 h-6">
+              <Image
+                src={"./hackers/arrow-right.svg"}
+                alt="Right Arrow"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
 
   const loadingStage = (
-    <div className={styles.loading_content_container}>
-      <Image src={sleepyFrog} alt={'sleepy frog'} className={styles.frog} />
-      <div className={styles.text_container}>
-        <h3>SEARCHING HIGH AND LOW...</h3>
-        <p>
+    <div className="h-full flex flex-col items-center justify-end px-[120px] pt-[250px] pb-20 gap-[150px] tablet-xl:px-[60px] tablet-xl:pt-[120px] tablet-xl:pb-[60px]0 tablet-s:px-10 tablet-s:pt-20 tablet-s:pb-10 tablet-s:gap-[60px]">
+      <Image
+        src={sleepyFrog}
+        alt="sleepy frog"
+        className="w-1/2 h-auto -translate-x-[5%]"
+      />
+      <div className="flex flex-col gap-3 text-black text-center font-jakarta px-6 tablet-s:px-2">
+        <h3 className="text-lg font-semibold tablet-s:text-sm">
+          SEARCHING HIGH AND LOW...
+        </h3>
+        <p className="text-lg font-normal tablet-s:text-sm">
           Please wait patiently while we match you to a judging table. Btw did
           you know next year will be HackDavis's 10 year anniversary?
         </p>
@@ -93,56 +110,60 @@ export default function TableNumberCheckin() {
   );
 
   const confirmStage = (
-    <div className={styles.confirm_content_container}>
-      <div className={styles.header}>
-        <h1>{tableNumber}</h1>
-        <div className={styles.text_container}>
-          <h3>YOUR TABLE NUMBER</h3>
-          <div className={styles.info_container}>
-            <Link
-              href={
-                'https://drive.google.com/file/d/1l6fxi9jDKlleaStt4xXSgCjVg4dfQkjz/view'
-              }
-              target="_blank"
-              className={styles.link}
-            >
-              Map Link{' '}
-              <Image
-                src={diagArrow}
-                alt="arrow"
-                className={styles.diag_arrow}
-              />
-            </Link>
-            <p>
-              Check that you and your team members received the same table
-              number. It is extremely important to be{' '}
-              <strong>present at your table</strong> when the judges arrive.
-            </p>
+    <div className="relative h-full flex flex-col items-center px-[120px] py-20 gap-10 tablet-xl:px-[90px] tablet-xl:py-[60px] tablet-xl:gap-8 tablet-l:px-10 tablet-l:py-10 tablet-l:gap-6 tablet-s:px-6 tablet-s:py-6">
+      {/* Header */}
+      <div className="flex flex-row-reverse h-min gap-20 tablet-xl:flex-col tablet-xl:gap-4">
+        <h1 className="text-[250px] font-light tablet-xl:text-center tablet-xl:text-[64px] tablet-xl:w-full">
+          {tableNumber}
+        </h1>
+        <div className="flex flex-col items-start text-left justify-between py-[60px] tablet-xl:py-0 tablet-xl:items-center">
+          <div className="flex flex-col gap-3 text-black font-jakarta tablet-xl:text-center">
+            <h3 className="text-lg font-semibold tablet-s:text-sm">
+              YOUR TABLE NUMBER
+            </h3>
+            <div className="flex flex-col gap-3 text-lg font-normal tablet-s:text-sm">
+              <Link
+                href="https://drive.google.com/file/d/1l6fxi9jDKlleaStt4xXSgCjVg4dfQkjz/view"
+                target="_blank"
+                className="underline underline-offset-4 flex flex-row mb-4"
+              >
+                Map Link <Image src={diagArrow} alt="arrow" className="ml-1" />
+              </Link>
+              <p>
+                Check that you and your team members received the same table
+                number. It is extremely important to be{" "}
+                <strong>present at your table</strong> when the judges arrive.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles.input_container}>
+
+      {/* Input Row */}
+      <div className="flex flex-row gap-11 w-full px-6 tablet-xl:gap-4 tablet-s:px-0">
         <input
           type="text"
-          className={styles.team_input}
+          className="rounded-2xl bg-white/70 border-transparent px-4 py-4 grow min-w-0 text-[#005271] text-lg border-none focus:outline-none focus:ring-0 placeholder:text-[#005271] disabled:opacity-60 tablet-s:text-sm tablet-s:py-2"
           placeholder="#####"
           value={teamNumber}
-          onChange={(event) => setTeamNumber(event.target.value)}
+          onChange={(e) => setTeamNumber(e.target.value)}
           disabled
         />
         <button
-          className={styles.yes_button}
+          className="text-white font-semibold text-lg flex justify-center items-center px-6 py-3 rounded-lg bg-[#005271] cursor-pointer grow disabled:opacity-30"
           disabled={!hasTeamNumber}
           onClick={() => setValue(tableNumber)}
         >
-          Yes! <Image src={modalArrow} alt="" />
+          Yes! <Image src={modalArrow} alt="" className="ml-2" />
         </button>
       </div>
+
+      {/* Not my team */}
       <button
-        className={styles.not_my_team_button}
+        className="text-white text-center text-lg font-semibold rounded-full opacity-50 bg-[#005271] px-6 py-1.5"
         onClick={() => {
           setTableNumber(null);
-          setTeamNumber('');
+          setTeamNumber("");
         }}
       >
         Wait! This is not my team
@@ -157,8 +178,8 @@ export default function TableNumberCheckin() {
   };
 
   return (
-    <div className={styles.background_dimmer}>
-      <div className={styles.modal_container}>{displayMap[stage]}</div>
+    <div className="fixed inset-0 bg-black/50 flex flex-col items-center justify-center px-[15px] z-[101]">
+      <div className="w-full">{displayMap[stage]}</div>
     </div>
   );
 }
