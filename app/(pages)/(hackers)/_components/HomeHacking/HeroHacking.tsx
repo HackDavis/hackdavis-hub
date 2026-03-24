9a9c528f43c06bbc1fd44faff742da1589000d3c
michelleyeoh <michellew.yeoh@gmail.com>
2026-03-23 17:50:58 -0700

    moved froggy star

--- app/(pages)/(hackers)/_components/HomeHacking/HeroHacking.tsx
+++ app/(pages)/(hackers)/_components/HomeHacking/HeroHacking.tsx
@@ -5,6 +5,7 @@ import { GoArrowRight } from 'react-icons/go';
 import Countdown from './_components/Countdown';
 import clouds from '@public/hackers/hero/Clouds.svg';
 import starLeft from '@public/hackers/hero/StarLeft.svg';
+import startRight from '@public/hackers/hero/StarRight.svg';
 
 interface HeroHackingProps {
   rolloutTime?: number;
@@ -62,7 +63,7 @@ export default function HeroHacking({
               {/* Decorative stars */}
               <Image
                 src={starLeft}
-                alt=""
+                alt="sparkling star"
                 width={120}
                 height={120}
                 className="
@@ -75,14 +76,14 @@ export default function HeroHacking({
               />
 
               <Image
-                src="/hackers/hero/StarRight.svg"
-                alt=""
+                src={startRight}
+                alt="sparkling star"
                 width={120}
                 height={120}
                 className="
                     pointer-events-none select-none
                     absolute z-20
-                    -right-2 top-[10%]
+                    -right-2 top-[0%]
                     w-[56px] sm:w-[70px] md:w-[84px] lg:w-[96px]
                     rotate-[10deg]
                   "
