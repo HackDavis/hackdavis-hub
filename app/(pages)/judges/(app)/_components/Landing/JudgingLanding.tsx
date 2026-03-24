ed11fb1c984fc9f9b125b3cf04491c01620df567
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-03-24 14:49:31 -0700

    Landing judingapp (#448)
    * spinning animation
    
    * updated hub hero
    
    * updated table locations
    
    * updated waiting cards
    
    * submit button + background/layout
    
    * pictures!!
    
    * rename files, delted unused, fix logout button, added judge name
    
    ---------
    
    Co-authored-by: michelleyeoh <michellew.yeoh@gmail.com>

--- app/(pages)/judges/(app)/_components/Landing/JudgingLanding.tsx
+++ app/(pages)/judges/(app)/_components/Landing/JudgingLanding.tsx
@@ -1,26 +1,24 @@
 'use client';
 
-import HubHero from './HubHero';
-import TableLocations from './TableLocations';
+import JudgeHero from './JudgeHero';
+import Questions from './Questions';
 import ViewProjects from './ViewProjects';
-import styles from './JudgingHub.module.scss';
 import Waiting from './Waiting';
 import PanelsAreLive from './PanelsAreLive';
-// import Dismiss from './Dismiss';
 import ClientTimeProtectedDisplay from '@pages/_components/TimeProtectedDisplay/ClientTimeProtectedDisplay';
 
-export default function JudgingHub() {
+export default function JudgingLanding() {
   return (
-    <div className={styles.container}>
+    <div className="w-full display flex-col bg-[#FAFAFF] px-[22px] pt-[63px] pb-[43px] flex gap-[40px]">
       <ClientTimeProtectedDisplay featureId="view-projects">
         <ViewProjects />
       </ClientTimeProtectedDisplay>
       <ClientTimeProtectedDisplay featureId="panels-are-live">
         <PanelsAreLive />
       </ClientTimeProtectedDisplay>
-      <HubHero />
+      <JudgeHero />
       <Waiting />
-      <TableLocations />
+      <Questions />
     </div>
   );
 }
