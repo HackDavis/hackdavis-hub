f2694aaae9e54059805c946748e66f8d1a25b4b3
michelleyeoh <michellew.yeoh@gmail.com>
2026-03-25 19:28:09 -0700

    added s to question

--- app/(pages)/judges/(app)/_components/Landing/Questions.tsx
+++ app/(pages)/judges/(app)/_components/Landing/Questions.tsx
@@ -4,7 +4,7 @@ import mascots_around_couch from '@public/judges/landing/mascots_around_couch.sv
 export default function Questions() {
   return (
     <div className="bg-white rounded-[32px] py-[34px] px-[30px] text-[#3F3F3F]">
-      <p className="font-semibold text-[22px]">Question?</p>
+      <p className="font-semibold text-[22px]">Questions?</p>
       <p className="text-[18px]">
         Please ask a HackDavis director (dark blue shirt)!
       </p>
