165b224695d55a576fd507efc21df68c01989b9d
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-04-07 16:27:50 -0700

    link section

--- app/(pages)/(hackers)/_components/StarterKit/Ideate.tsx
+++ app/(pages)/(hackers)/_components/StarterKit/Ideate.tsx
@@ -34,6 +34,33 @@ import timeline from "@public/hackers/starter-kit/ideate/timeline.svg";
 // Extra
 import scroll_bar1 from "@public/hackers/starter-kit/ideate/scroll_bar1.svg";
 import divider from "@public/hackers/starter-kit/ideate/divider.svg";
+
+interface Link {
+  type: string;
+  title: string;
+  description: string;
+  image: StaticImageData;
+  buttonName: string; 
+  link: string;
+  alt: string;
+}
+
+function Link({link} : {link: Link}) {
+  return (
+    <div className="flex flex-col md:flex-row bg-white rounded-[16px] gap-[32px] px-[18px] py-[32px]">
+      <Image src={link.image} alt={link.alt} className="w-full md:w-auto" />
+      <div className="flex flex-col gap-[8px]">
+        <p className="opacity-[0.40] text-[16px]">{link.type}</p>
+        <p className="text-[20px] text-[#1F1F1F] font-semibold">{link.title}</p>
+        <p className="opacity-[0.65] text-[16px]">{link.description}</p>
+        <button className="flex flex-row gap-[4px] items-center bg-[#3F3F3F] rounded-full px-[24px] py-[12px] mt-[8px] text-[#FAFAFF] text-[14px] w-min text-nowrap" onClick={() => window.open(link.link, "_blank")}>
+          {link.buttonName} 
+          <Image src={arrow} alt="arrow" className="w-[20px] h-[20px] shrink-0"/>
+        </button> 
+      </div>
+    </div>
+  );
+}
 export default function Ideate() {
   return <div>Ideate</div>;
 }
