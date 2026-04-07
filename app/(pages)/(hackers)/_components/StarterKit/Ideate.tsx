6ed38c8f1e48d710f16fb0fbefbd684fde254b9b
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-04-07 16:34:43 -0700

    i forgot to change the font for link

--- app/(pages)/(hackers)/_components/StarterKit/Ideate.tsx
+++ app/(pages)/(hackers)/_components/StarterKit/Ideate.tsx
@@ -52,7 +52,7 @@ function Link({link} : {link: Link}) {
     <div className="flex flex-col md:flex-row bg-white rounded-[16px] gap-[32px] px-[18px] py-[32px]">
       <Image src={link.image} alt={link.alt} className="w-full md:w-auto" />
       <div className="flex flex-col gap-[8px]">
-        <p className="opacity-[0.40] text-[16px]">{link.type}</p>
+        <p className="opacity-[0.40] text-[16px] font-mono">{link.type}</p>
         <p className="text-[20px] text-[#1F1F1F] font-semibold">{link.title}</p>
         <p className="opacity-[0.65] text-[16px]">{link.description}</p>
         <button className="flex flex-row gap-[4px] items-center bg-[#3F3F3F] rounded-full px-[24px] py-[12px] mt-[8px] text-[#FAFAFF] text-[14px] w-min text-nowrap" onClick={() => window.open(link.link, "_blank")}>
@@ -63,6 +63,7 @@ function Link({link} : {link: Link}) {
     </div>
   );
 }
+
 function Resources({title, link1, link2} : {title: string, link1: Link, link2: Link}) {
   return (
     <div className="flex flex-col gap-[16px]">
