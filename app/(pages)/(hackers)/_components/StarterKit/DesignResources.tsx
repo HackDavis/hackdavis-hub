269baab8dcd8b3ad71d1178a385120aec1f420db
michelleyeoh <michellew.yeoh@gmail.com>
2026-04-07 21:20:06 -0700

    moved images to new folder

--- app/(pages)/(hackers)/_components/StarterKit/DesignResources.tsx
+++ app/(pages)/(hackers)/_components/StarterKit/DesignResources.tsx
@@ -1,41 +1,41 @@
 'use client';
 
 import Image from 'next/image';
-import type { StaticImageData } from 'next/image'; 
+import type { StaticImageData } from 'next/image';
 import { useState, useRef } from 'react';
 
 // Icons
-import writing_sign from '@public/hackers/starter-kit/ideate/tabler_writing-sign.svg';
-import presentation_analytics from '@public/hackers/starter-kit/ideate/tabler_presentation-analytics.svg';
-import sparkles from '@public/hackers/starter-kit/ideate/tabler_sparkles-2.svg';
-import figma from '@public/hackers/starter-kit/ideate/tabler_brand-figma.svg';
+import writing_sign from '@public/hackers/starter-kit/designResources/tabler_writing-sign.svg';
+import presentation_analytics from '@public/hackers/starter-kit/designResources/tabler_presentation-analytics.svg';
+import sparkles from '@public/hackers/starter-kit/designResources/tabler_sparkles-2.svg';
+import figma from '@public/hackers/starter-kit/designResources/tabler_brand-figma.svg';
 
-import github from '@public/hackers/starter-kit/ideate/tabler_brand-github.svg';
-import desktop_code from '@public/hackers/starter-kit/ideate/tabler_device-desktop-code.svg';
-import message_chatbot from '@public/hackers/starter-kit/ideate/tabler_message-chatbot.svg';
+import github from '@public/hackers/starter-kit/designResources/tabler_brand-github.svg';
+import desktop_code from '@public/hackers/starter-kit/designResources/tabler_device-desktop-code.svg';
+import message_chatbot from '@public/hackers/starter-kit/designResources/tabler_message-chatbot.svg';
 
-import text_size from '@public/hackers/starter-kit/ideate/tabler_text-size.svg';
-import image_in_picture from '@public/hackers/starter-kit/ideate/tabler_image-in-picture.svg';
-import shape from '@public/hackers/starter-kit/ideate/tabler_shape.svg';
+import text_size from '@public/hackers/starter-kit/designResources/tabler_text-size.svg';
+import image_in_picture from '@public/hackers/starter-kit/designResources/tabler_image-in-picture.svg';
+import shape from '@public/hackers/starter-kit/designResources/tabler_shape.svg';
 
-import arrow from '@public/hackers/starter-kit/ideate/arrow_icon.svg';
+import arrow from '@public/hackers/starter-kit/designResources/arrow_icon.svg';
 
 // Big Images
-import designer_responsibilities_image from '@public/hackers/starter-kit/ideate/designer_responsibilities.svg';
-import developer_responsibilities_image from '@public/hackers/starter-kit/ideate/developer_responsibilities.svg';
-import designer_tips1_image from '@public/hackers/starter-kit/ideate/designer_tips1.svg';
-import designer_tips2_image from '@public/hackers/starter-kit/ideate/designer_tips2.svg';
+import designer_responsibilities_image from '@public/hackers/starter-kit/designResources/designer_responsibilities.svg';
+import developer_responsibilities_image from '@public/hackers/starter-kit/designResources/developer_responsibilities.svg';
+import designer_tips1_image from '@public/hackers/starter-kit/designResources/designer_tips1.svg';
+import designer_tips2_image from '@public/hackers/starter-kit/designResources/designer_tips2.svg';
 
-import figma_beginners_image from '@public/hackers/starter-kit/ideate/figma_beginners.svg';
-import figma_shapes from '@public/hackers/starter-kit/ideate/figma_shapes.svg';
-import mongodb from '@public/hackers/starter-kit/ideate/mongodb.svg';
-import auth0 from '@public/hackers/starter-kit/ideate/auth0.svg';
+import figma_beginners_image from '@public/hackers/starter-kit/designResources/figma_beginners.svg';
+import figma_shapes from '@public/hackers/starter-kit/designResources/figma_shapes.svg';
+import mongodb from '@public/hackers/starter-kit/designResources/mongodb.svg';
+import auth0 from '@public/hackers/starter-kit/designResources/auth0.svg';
 
-import timeline from '@public/hackers/starter-kit/ideate/timeline.svg';
+import timeline from '@public/hackers/starter-kit/designResources/timeline.svg';
 
 // Extra
-//import scroll_bar1 from '@public/hackers/starter-kit/ideate/scroll_bar1.svg';
-//import divider from '@public/hackers/starter-kit/ideate/divider.svg';
+//import scroll_bar1 from '@public/hackers/starter-kit/designResources/scroll_bar1.svg';
+//import divider from '@public/hackers/starter-kit/designResources/divider.svg';
 
 interface Link {
   type: string;
