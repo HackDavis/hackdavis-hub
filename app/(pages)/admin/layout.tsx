b64e513d57bb1c634592bf95836a111c7be9f5aa
michelleyeoh <michellew.yeoh@gmail.com>
2026-05-04 23:15:59 -0700

    fix build error

--- app/(pages)/admin/layout.tsx
+++ app/(pages)/admin/layout.tsx
@@ -24,7 +24,7 @@ export default function AdminLayout({
   return (
     <ProtectedDisplay
       allowedRoles={['admin']}
-      allowedUser={[parsedAdminEmail]}
+      allowedUser={parsedAdminEmail}
       failRedirectRoute="/login"
     >
       {children}
