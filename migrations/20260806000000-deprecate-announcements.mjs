06f5f72e6ecf8e3729b64672c3ef3cb761485b78
Haylie Tan <haytan@ucdavis.edu>
2026-08-05 20:28:09 -0700

    chore: add migration to deprecate announcements collection

---
+++ migrations/20260806000000-deprecate-announcements.mjs
@@ -0,0 +1,40 @@
+export const up = async (db) => {
+  const collectionExists = await db
+    .listCollections({ name: "announcements" }, { nameOnly: true })
+    .hasNext();
+
+  if (collectionExists) {
+    await db.collection("announcements").drop();
+  }
+};
+
+export const down = async (db) => {
+  await db.createCollection("announcements", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "Announcements Object Validation",
+        required: ["title", "description", "time"],
+        properties: {
+          _id: {
+            bsonType: "objectId",
+            description: "_id must be an ObjectId",
+          },
+          title: {
+            bsonType: "string",
+            description: "title must be a string",
+          },
+          description: {
+            bsonType: "string",
+            description: "description must be a string",
+          },
+          time: {
+            bsonType: "date",
+            description: "time must be a date.",
+          },
+        },
+        additionalProperties: false,
+      },
+    },
+  });
+};
